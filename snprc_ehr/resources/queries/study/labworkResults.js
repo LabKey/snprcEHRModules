65789fc245353aa51c97ad5e51311f70291713a8
Terry Hawkins <thawkins@txbiomed.org>
2015-03-18 17:43:37 +0000

    snprc_ehr - ETL updates

---
+++ snprc_ehr/resources/queries/study/labworkResults.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2010-2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+require("ehr/triggers").initScript(this);
+
+function onInit(event, helper){
+    helper.setScriptOptions({
+        allowAnyId: true,
+        allowDeadIds: true,
+        skipIdFormatCheck: true,
+        removeTimeFromDate: false,
+        allowDatesInDistantPast: true
+
+    });
+}
+
+function onUpsert(helper, scriptErrors, row, oldRow){
+    //lookup test type if not supplied:
+    if (row.servicerequested){
+        if (!row.type)
+            row.type = helper.getJavaHelper().lookupDatasetForService(row.servicerequested);
+
+        if (!row.chargetype)
+            row.chargetype = helper.getJavaHelper().lookupChargeTypeForService(row.servicerequested);
+    }
+}
