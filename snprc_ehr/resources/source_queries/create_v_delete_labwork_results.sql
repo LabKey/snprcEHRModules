4cf7b93c3bebc6342a2b70df2f6d5d26fa070702
Terry Hawkins <thawkins@txbiomed.org>
2015-06-29 16:18:44 +0000

    snprc_ehr - updated ETLs and source queries.

--- snprc_ehr/resources/source_queries/create_v_delete_labwork_results.sql
+++ snprc_ehr/resources/source_queries/create_v_delete_labwork_results.sql
@@ -1,7 +1,7 @@
 USE [animal]
 GO
 
-/****** Object:  View [dbo].[Labkey_etl.v_delete_clinPathRuns]    Script Date: 6/26/2015 2:02:00 PM ******/
+/****** Object:  View [dbo].[Labkey_etl.v_delete_labwork_results]    Script Date: 6/26/2015 2:02:00 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,17 +9,17 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-ALTER VIEW Labkey_etl.v_delete_clinPathRuns AS
+CREATE VIEW Labkey_etl.v_delete_labwork_results AS
 -- ==========================================================================================
 -- Author:		Terry Hawkins
 -- Create date: 6/26/2015
--- Description:	Selects the ETL records for LabKey study.clinPathRuns dataset for deletes
+-- Description:	Selects the ETL records for LabKey study.labwork_results dataset for deletes
 -- Changes:
 --
 --
 -- ==========================================================================================
 SELECT log.MESSAGE_ID,
-log.ENTRY_DATE_TM
+	   log.ENTRY_DATE_TM
 
 FROM dbo.HL7_IMPORT_LOG AS log
 WHERE log.RESULT_STATUS = 'X' -- 'X' = cancelled orders
@@ -27,7 +27,7 @@ WHERE log.RESULT_STATUS = 'X' -- 'X' = cancelled orders
 
 GO
 
-GRANT SELECT ON Labkey_etl.v_delete_clinPathRuns TO z_labkey
+GRANT SELECT ON Labkey_etl.v_delete_labwork_results TO z_labkey
 GRANT SELECT ON dbo.HL7_IMPORT_LOG TO z_labkey
   
 GO
