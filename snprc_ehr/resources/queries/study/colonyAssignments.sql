d88fa5c30ce78274f482e6fe4ee09a6c857dc53f
Terry Hawkins <thawkins@txbiomed.org>
2015-08-31 17:22:06 +0000

    snprc_ehr - Added animal group ETLs and report queries for colonies and breeding groups

---
+++ snprc_ehr/resources/queries/study/colonyAssignments.sql
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2015 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+SELECT
+
+agm.id,
+agm.date,
+agm.enddate,
+ag.name,
+ag.comment
+
+FROM ehr.animal_group_members as agm
+JOIN ehr.animal_groups ag on (ag.category = 'Colony' and agm.groupId = ag.rowid)
