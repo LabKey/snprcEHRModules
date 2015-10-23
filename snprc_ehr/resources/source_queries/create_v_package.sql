a978ec1d105c793bec190ed239ca8610606f4817
Terry Hawkins <thawkins@txbiomed.org>
2015-10-23 18:43:12 +0000

    snprc_ehr - fixed copy/paste typo

--- snprc_ehr/resources/source_queries/create_v_package.sql
+++ snprc_ehr/resources/source_queries/create_v_package.sql
@@ -21,25 +21,18 @@ ALTER VIEW [labkey_etl].[v_package] AS
 -- ==========================================================================================
 
 
-SELECT pc.TID AS id,
-	pc.pkg_id AS packageId,
-	CAST(vct.code AS INT) AS categoryId,
-	pc.user_name AS user_name,
-	pc.entry_date_tm AS entry_date_tm,
-	pc.object_id AS objectid,
-	pc.timestamp AS timestamp
-FROM dbo.PKG_CATEGORY AS pc
-INNER JOIN valid_code_table AS vct ON pc.CATEGORY_CODE = vct.CODE 
-				AND vct.TABLE_NAME = 'pkg_category'
-				AND vct.COLUMN_NAME = 'category_code'
-				AND vct.END_DATE_TM IS null
+SELECT p.PKG_ID AS id,
+	LEFT(p.DESCRIPTION, 100) AS name,
+	p.NARRATIVE AS descriptiong,
+	p.user_name AS user_name,
+	p.entry_date_tm AS entry_date_tm,
+	p.object_id AS objectid,
+	p.timestamp AS timestamp
+FROM dbo.PKGS AS p
 
 
 GO
-grant SELECT on [labkey_etl].[v_package_category_junction] to z_labkey
+grant SELECT on [labkey_etl].[v_package] to z_labkey
 
 go
 
-
-SELECT * FROM labkey_etl.v_package_category_junction
-SELECT * FROM labkey_etl.v_package_category
