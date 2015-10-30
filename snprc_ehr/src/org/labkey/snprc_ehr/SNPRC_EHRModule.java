8c9e2a74c782662fb6b48c6a3a03779584ca22b6
Adam Rauch <adamr@labkey.com>
2015-10-30 16:42:13 +0000

    Consolidate scripts and bump module versions for 15.30 release (except for Argos and Adjudication)

--- snprc_ehr/src/org/labkey/snprc_ehr/SNPRC_EHRModule.java
+++ snprc_ehr/src/org/labkey/snprc_ehr/SNPRC_EHRModule.java
@@ -49,7 +49,7 @@ public class SNPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public double getVersion()
     {
-        return 15.22;
+        return 15.30;
     }
 
     @Override
