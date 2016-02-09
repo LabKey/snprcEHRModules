bead98f5aff005f92a4f74db18cd953c4207a43a
Terry Hawkins <thawkins@txbiomed.org>
2016-02-09 17:58:41 +0000

    snprc_ehr - Animal History->Daily Obs report reformatted, and supporting foreign key lookup table (housingStatus) added.

--- snprc_ehr/resources/queries/study/dailyObsPivot.sql
+++ snprc_ehr/resources/queries/study/dailyObsPivot.sql
@@ -16,8 +16,23 @@
 SELECT
 c.id,
 c.date,
-c.category,
-group_concat(cast(c.observation as varchar)) as observations
+c."water::observation" as water,
+c."feed::observation" as feed,
+
+case when c."sa_none::observation" = 'Y' then 'None' else '' end +
+case	when c."sa_unknown::observation" = 'Y' then 'Unknown' else '' end +
+case	when c."sa_normal::observation" = 'Y' then 'Normal' else '' end +
+case when c."sa_bloody::observation" = 'Y' then ' Bloody' else '' end +
+case when c."sa_dry::observation" = 'Y' then ' Dry' else '' end +
+case when c."sa_loose::observation" = 'Y' then ' Loose' else '' end +
+case when c."sa_other::observation" = 'Y' then ' Other' else '' end +
+case when c."sa_soft::observation" = 'Y' then ' Soft' else '' end +
+case when c."sa_pellet::observation" = 'Y' then ' Pellet' else '' end +
+case when c."sa_watery::observation" = 'Y' then ' Watery' else '' end as stool,
+COALESCE (c."housing_status::observation", '0') as housing_status,
+COALESCE (c."comments::observation", ' ') as comments,
+c.performedby
+
 
 FROM (
 
@@ -25,17 +40,18 @@ SELECT
   co.id,
   co.date,
   LEFT(co.objectid, 37 ) as objectid,
-  co.category,
-  co.observation
+  co.category as category,
+  max(co.observation) as observation,
+  max(performedby) as performedby
+
 FROM study.clinical_observations co
 INNER JOIN snprc_ehr.clinical_observation_datasets as cod on co.category = cod.category_name and cod.dataset_name = 'daily_obs'
 WHERE co.qcstate.publicdata = true
-) c
 
-GROUP BY c.id, c.date, c.category, c.objectid
+GrOUP BY co.id, co.date, LEFT(co.objectid, 37 ), co.category, performedby
 
-PIVOT  observations BY category IN
-(select category_name from snprc_ehr.clinical_observation_datasets where dataset_name = 'daily_obs' order by sort_order)
--- (	'water', 'feed','sa_none', 'sa_bloody','sa_dry', 'sa_loose', 'sa_normal', 'sa_other',
---	   'sa_pellet', 'sa_soft' ,  'sa_unknown','sa_watery', 'housing_status', 'comments')
+PIVOT  observation BY category IN
+   (select category_name from snprc_ehr.clinical_observation_datasets where dataset_name = 'daily_obs' order by sort_order)
+ ) as c
+
 order by id, date desc
--- snprc_ehr/resources/source_queries/create_v_observations.sql
+++ snprc_ehr/resources/source_queries/create_v_observations.sql
@@ -38,18 +38,19 @@ SELECT id,
 	obs_date AS date, 
 	CAST(object_id AS VARCHAR(128)) + '/' + category AS objectid,
 	category, 
-	CAST(observation AS VARCHAR(3)) AS observation,
+	CAST(observation AS varchar(80)) AS observation,
 	user_name AS performedby,
 	timestamp 
 FROM (SELECT id, obs_date,
-	CAST(water AS VARCHAR(3) ) AS water, CAST(feed  AS VARCHAR(3) ) AS feed,
-	CAST(sa_none AS VARCHAR(3) ) AS sa_none,  CAST(sa_bloody AS VARCHAR(3)) AS sa_bloody,
-	CAST(sa_dry AS VARCHAR(3) ) AS sa_dry, CAST(sa_loose AS VARCHAR(3) ) AS sa_loose,
-	CAST(sa_normal AS VARCHAR(3) ) AS sa_normal, CAST(sa_other AS VARCHAR(3) ) AS sa_other,
-	CAST(sa_pellet AS VARCHAR(3) ) AS sa_pellet,
-	CAST(sa_soft AS VARCHAR(3) ) AS sa_soft, CAST(sa_unknown AS VARCHAR(3) ) AS sa_unknown, 
-	CAST(sa_watery  AS VARCHAR(3) ) AS sa_watery, 
-	CAST(housing_status AS VARCHAR(3)) AS housing_status,
+	CAST(water AS varchar(80) ) AS water, CAST(feed  AS varchar(80) ) AS feed,
+	CAST(sa_none AS varchar(80) ) AS sa_none,  CAST(sa_bloody AS varchar(80)) AS sa_bloody,
+	CAST(sa_dry AS varchar(80) ) AS sa_dry, CAST(sa_loose AS varchar(80) ) AS sa_loose,
+	CAST(sa_normal AS varchar(80) ) AS sa_normal, CAST(sa_other AS varchar(80) ) AS sa_other,
+	CAST(sa_pellet AS varchar(80) ) AS sa_pellet,
+	CAST(sa_soft AS varchar(80) ) AS sa_soft, CAST(sa_unknown AS varchar(80) ) AS sa_unknown, 
+	CAST(sa_watery  AS varchar(80) ) AS sa_watery, 
+	CAST(housing_status AS varchar(80)) AS housing_status,
+	CAST(comments AS varchar(80)) AS comments,
 	o.object_id,
     o.user_name ,
     o.entry_date_tm ,
@@ -61,7 +62,7 @@ AS result
 UNPIVOT
 (
   observation FOR category   IN (water, feed , sa_none, sa_bloody, sa_dry, sa_loose,
-	sa_normal, sa_other, sa_pellet, sa_soft, sa_unknown, sa_watery, housing_status)
+	sa_normal, sa_other, sa_pellet, sa_soft, sa_unknown, sa_watery, housing_status, comments)
 	)
 	AS Unpvt
 
