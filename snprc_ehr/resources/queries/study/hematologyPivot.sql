7fd8f9a03cc955cd6f5d043d66e940bc70c443e5
Karl Lum <klum@labkey.com>
2016-11-10 02:19:16 +0000

    merge from modules16.3 through 47583

--- snprc_ehr/resources/queries/study/hematologyPivot.sql
+++ snprc_ehr/resources/queries/study/hematologyPivot.sql
@@ -4,30 +4,55 @@
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
 SELECT
-b.id,
-b.date,
-group_concat(distinct servicerequested, chr(10)) as servicerequested,
-b.TestName,
-group_concat(b.result) as results
-
+x.*,
+y.servicerequested
 FROM (
-
 SELECT
   b.id,
   b.date,
-  b.testId.name AS TestName,
-  b.runid.servicerequested as servicerequested,
-  coalesce(b.runId, b.objectid) as runId,
-  b.resultoorindicator,
-  CASE
-  WHEN b.result IS NULL THEN  b.qualresult
-    ELSE CAST(CAST(b.result AS float) AS VARCHAR)
-  END as result
-FROM study.labworkResults b
-WHERE b.testId.includeInPanel = true AND b.qcstate.publicdata = true and b.testid.type = 'Hematology'
-) b
+  b.runId,
+  b.TestName,
+  group_concat(b.result) as results
 
-GROUP BY b.id, b.date, b.runId, b.TestName
-PIVOT results BY TestName IN
-(select name from snprc_ehr.lab_tests t WHERE t.includeInPanel = true AND Type='Hematology')
+  FROM (
+
+  SELECT
+    b.id,
+    b.date,
+    b.testId.name AS TestName,
+    coalesce(b.runId, b.objectid) as runId,
+    b.resultoorindicator,
+    CASE
+    WHEN b.result IS NULL THEN  b.qualresult
+      ELSE CAST(CAST(b.result AS float) AS VARCHAR)
+    END as result
+  FROM study.labworkResults b
+  WHERE b.testId.includeInPanel = true AND b.qcstate.publicdata = true and b.testid.type = 'Hematology'
+  ) b
+
+  GROUP BY b.id, b.date, b.runId, b.TestName
+  PIVOT results BY TestName IN
+  (select name from snprc_ehr.lab_tests t WHERE t.includeInPanel = true AND Type='Hematology')
+) X
+INNER JOIN (
+SELECT
+  b.id,
+  b.date,
+  group_concat(distinct servicerequested, chr(10)) as servicerequested,
+  b.runId
+  FROM (
+
+  SELECT
+    b.id,
+    b.date,
+    b.testId.name AS TestName,
+    b.runid.servicerequested AS servicerequested,
+    coalesce(b.runId, b.objectid) as runId
+  FROM study.labworkResults b
+  WHERE b.testId.includeInPanel = true AND b.qcstate.publicdata = true and b.testid.type = 'Hematology'
+  ) b
+
+  GROUP BY b.id, b.date, b.runId
+) Y
+ON X.id = Y.id AND X.date = Y.date AND X.runId = Y.runId
 
