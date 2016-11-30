26327c29da4c53ffc5c5a37a0148ee33dcd0ae20
Josh Eckels <jeckels@labkey.com>
2016-11-30 21:55:34 +0000

    Spec Issue 28279 - [SNPRC] Plotting for text values that are Mostly numbers
    Mark subquery expression as a measure by pulling it into a separate query

--- snprc_ehr/resources/queries/study/chemPivot.sql
+++ snprc_ehr/resources/queries/study/chemPivot.sql
@@ -7,24 +7,9 @@ SELECT
 b.Id,
 b.date,
 b.TestName,
-group_concat(b.result) as results
+MAX(b.result) as results
 
-FROM (
-
-SELECT
-  b.Id,
-  b.date,
-  b.testId.name AS TestName,
-  coalesce(b.runId, b.objectid) as runId,
-  b.resultoorindicator,
-  CASE
-  WHEN b.result IS NULL THEN  b.qualresult
-    ELSE CAST(CAST(b.result AS float) AS VARCHAR)
-  END as result
-FROM study.labworkResults b
-WHERE b.testId.includeInPanel = true and b.qcstate.publicdata = true and b.testid.type = 'Biochemistry'
-
-) b
+FROM chemPivotInner b
 
 GROUP BY b.runid,b.id, b.date, b.TestName
 
