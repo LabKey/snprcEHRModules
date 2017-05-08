c783fffc7183a40c790bb9521bbfc48c181db440
Cory Nathe <cnathe@labkey.com>
2017-05-08 16:33:53 +0000

    PopulationPanel.js refactor: change prc specific panels to extend EHR.panel.PopulationPanel from base ehr module - this allows us to apply the number format (right justified) and header styling change more broadly

--- snprc_ehr/resources/web/snprc_ehr/Panel/ColonyOverviewPanel.js
+++ snprc_ehr/resources/web/snprc_ehr/Panel/ColonyOverviewPanel.js
@@ -3,8 +3,8 @@
  *
  * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
  */
-Ext4.define('EHR.panel.ColonyOverviewPanel', {
-    extend: 'Ext.panel.Panel',
+Ext4.define('SNPRC_EHR.panel.ColonyOverviewPanel', {
+    extend: 'EHR.panel.ColonyOverviewPanel',
 
     initComponent: function(){
         this.filterArray = [
@@ -54,51 +54,5 @@ Ext4.define('EHR.panel.ColonyOverviewPanel', {
         }
 
         this.callParent();
-    },
-
-    getTabs: function(){
-        return [{
-            title: 'Population Composition',
-            style: 'padding 5px;',
-            items: [{
-                xtype: 'ehr-populationpanel',
-                filterArray: this.filterArray,
-                rowField: EHR.panel.PopulationPanel.FIELDS.species,
-                colFields: [EHR.panel.PopulationPanel.FIELDS.ageclass, EHR.panel.PopulationPanel.FIELDS.gender]
-            }],
-            itemId: 'population'
-        },{
-            title: 'SPF Colony',
-            style: 'padding 5px;',
-            items: [{
-                xtype: 'ehr-populationpanel',
-                titleText: 'SPF 4 (SPF)',
-                filterArray: [LABKEY.Filter.create('Id/viral_status/viralStatus', 'SPF 4', LABKEY.Filter.Types.EQUALS)].concat(this.filterArray),
-                rowField: EHR.panel.PopulationPanel.FIELDS.species,
-                colFields: [EHR.panel.PopulationPanel.FIELDS.ageclass, EHR.panel.PopulationPanel.FIELDS.gender]
-            },{
-                xtype: 'ehr-populationpanel',
-                titleText: 'SPF 9 (ESPF)',
-                filterArray: [LABKEY.Filter.create('Id/viral_status/viralStatus', 'SPF 9', LABKEY.Filter.Types.EQUALS)].concat(this.filterArray),
-                rowField: EHR.panel.PopulationPanel.FIELDS.species,
-                colFields: [EHR.panel.PopulationPanel.FIELDS.ageclass, EHR.panel.PopulationPanel.FIELDS.gender]
-            }],
-            itemId: 'spf'
-        },{
-            title: 'Housing Summary',
-            xtype: 'ehr-housingsummarypanel',
-            itemId: 'housingSummary'
-        },{
-            title: 'Utilization Summary',
-            xtype: 'ehr-utilizationsummarypanel',
-            filterArray: this.filterArray,
-            itemId: 'utilizationSummary'
-        },{
-            title: 'Clinical Case Summary',
-            xtype: 'ehr-clinicalsummarypanel',
-            demographicsFilterArray: this.filterArray,
-            filterArray: this.childFilterArray,
-            itemId: 'clinicalSummary'
-        }];
     }
 });
