bc73247383ee81442443a1ee8c5ac5c28003f671
Terry Hawkins <thawkins@txbiomed.org>
2017-11-15 22:18:15 +0000

    snprc_r24 - Datasets, schema, and query updates

--- snprc_ehr/resources/queries/study/R24DemographicsSrc.sql
+++ snprc_ehr/resources/queries/study/R24DemographicsSrc.sql
@@ -14,15 +14,37 @@
  * limitations under the License.
  */
 SELECT
-  d.id as animalId,
+  d.participantid as animalId,
+  ih.value as neprcId,
   d.birth,
   d.death,
   d.calculated_status.code as status,
   d.objectid,
   d.species.arc_species_code.code as species,
-  'SNPRC' as sourceColony,
+  -- Animals born at NEPRC and animals with both parents born at NEPRC
+  -- have NEPRC as the sourceColony.  All others have SNPRC.
+  CASE WHEN ih.id is NOT NULL then 'NEPRC'
+	ELSE CASE WHEN  iDam.id IS NOT NULL AND iSire.id IS NOT null
+			  THEN 'NEPRC'
+    ELSE 'SNPRC' END
+  END AS sourceColony,
+
+  'SNPRC' as currentColony,
   d.gender.code as gender,
   d.dam,
   d.sire
 FROM study.demographics d
-where species.arc_species_code = 'CJ'
+
+-- is participant from NEPRC?
+LEFT OUTER JOIN study.idhistory as ih on d.id = ih.id and ih.id_type in
+	(select max(ii.id_type) from study.idhistory as ii where ii.id = ih.id
+	and ih.id_type in (11, 28)) --NEPRC id types
+-- is dam from NEPRC?
+LEFT OUTER JOIN study.idhistory as iDam ON iDam.id = d.dam and iDam.id_type IN
+		(select max(ii.id_type) from study.idhistory as ii where ii.id = iDam.id
+					AND ii.id_type in (11, 28))
+-- is sire from NEPRC?
+LEFT OUTER JOIN study.idhistory as iSire ON iSire.id = d.sire and iSire.id_type IN
+		(select max(ii.id_type) from study.idhistory as ii where ii.id = iSire.id
+					AND ii.id_type in (11, 28))
+where d.species.arc_species_code = 'CJ'
