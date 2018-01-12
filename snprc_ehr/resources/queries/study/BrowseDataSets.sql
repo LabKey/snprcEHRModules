531c58f9a842178ea32b6ad841082100d747a827
Terry Hawkins <thawkins@txbiomed.org>
2018-01-12 21:45:41 +0000

    snprc - tweaked datasets.html to support non-study datasets

---
+++ snprc_ehr/resources/queries/study/BrowseDataSets.sql
@@ -0,0 +1,15 @@
+select 'study' as Schema,
+    categoryId.label as CategoryId,
+    Label as Label,
+    Name as name,
+    ShowByDefault as ShowByDefault,
+    true as isAnimal
+from study.datasets
+UNION
+  select 'study' as schema, 'ClinPath' as CategoryId,'Surveillance Results' as Label, 'surveillancePivot' as Name,  true as ShowByDefault, true as isAnimal
+UNION
+select 'snprc_ehr' as schema, 'Misc' as CategoryId,'Fee Schedule' as Label, 'FeeSchedulePivot' as Name,  true as ShowByDefault, false as isAnimal
+UNION
+select 'ehr_lookups' as schema, 'Lookup Tables' as CategoryId,'Lookup sets' as Label, 'lookup_sets' as Name,  true as ShowByDefault, false as isAnimal
+UNION
+select 'snprc_ehr' as schema, 'Misc' as CategoryId,'Valid Charge by Species' as Label, 'ValidChargeBySpecies' as Name,  true as ShowByDefault, false as isAnimal
