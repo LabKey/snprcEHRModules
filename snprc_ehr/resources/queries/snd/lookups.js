8ac309505c0a0132a4cc345ad8b2b927d49e4800
Terry Hawkins <thawkins@txbiomed.org>
2018-02-16 20:33:02 +0000

    snprc - added trigger script for snd.lookups table (need to populate lookupSetId)

---
+++ snprc_ehr/resources/queries/snd/lookups.js
@@ -0,0 +1,50 @@
+//
+// require("ehr/triggers").initScript(this);
+//
+// function onInit(event, helper){
+//     helper.setScriptOptions({
+//         allowFutureDates: true
+//     });
+// }
+
+/*  animal_groups table moved to snprc_ehr schema and no longer uses RowId as the foreign key
+    Code commented out, but please do not delete - it may be uncommented in the future.  tjh
+
+var cachedGroups = {};
+
+function onUpsert(helper, scriptErrors, row, oldRow){
+    // If we don't have the RowId of the group, try resolving it based on the category and name
+    if(!row.GroupId && row.GroupCategory && row.GroupName) {
+        var cacheKey = row.GroupCategory + '~~~~~' + row.GroupName;
+        row.GroupId = cachedGroups[cacheKey];
+        if (row.GroupId === undefined || row.GroupId == 'undefined')
+        {
+            LABKEY.Query.selectRows({
+                schemaName: 'ehr',
+                queryName: 'animal_groups',
+                columns: 'RowId',
+                scope: this,
+                filterArray: [
+                    LABKEY.Filter.create('Category', row.GroupCategory, LABKEY.Filter.Types.EQUAL),
+                    LABKEY.Filter.create('Name', row.GroupName, LABKEY.Filter.Types.EQUAL)
+                ],
+                success: function (data)
+                {
+                    if (data.rows && data.rows.length)
+                    {
+                        row.GroupId = data.rows[0].rowid;
+                        cachedGroups[cacheKey] = row.GroupId;
+                        console.log('caching ' + cacheKey + ': ' + row.GroupId);
+                    }
+                },
+                failure: function (error)
+                {
+                    console.log('Select rows error');
+                    console.log(error);
+                }
+            });
+        }
+
+    }
+}
+ */
