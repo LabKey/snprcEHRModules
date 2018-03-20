19c7930a911e723f197a7f5c0f805636bb113a4f
Terry Hawkins <thawkins@txbiomed.org>
2018-03-20 15:13:14 +0000

    snprc - More housing tweaks

--- snprc_ehr/resources/source_queries/create_v_housing.sql
+++ snprc_ehr/resources/source_queries/create_v_housing.sql
@@ -1,23 +1,13 @@
-/*
- * Copyright (c) 2015-2017 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+USE [animal]
+GO
+
+/****** Object:  View [labkey_etl].[V_HOUSING]    Script Date: 3/16/2018 11:48:35 AM ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+
 
 
 
@@ -30,17 +20,17 @@ ALTER VIEW [labkey_etl].[V_HOUSING] AS
 -- 11/11/2016  added modified, modifiedby, created, and createdby columns. tjh
 -- 12/20/2017  query now sources function that combines locations and cage positions. tjh
 -- ==========================================================================================
-SELECT f.id,
+SELECT f.move_type,f.id,
   f.startDate AS date,
-  COALESCE(LAG(f.startDate) OVER (PARTITION BY f.id ORDER BY f.startDate DESC, f.timestamp asc), cd.disp_date_tm_max) AS endDate,
+  COALESCE(LAG(f.startDate) OVER (PARTITION BY f.id ORDER BY f.startDate DESC, f.timestamp ASC), cd.disp_date_tm_max) AS endDate,
   CAST(f.room AS VARCHAR(10)) AS room,
   f.cage,
   f.group_housing_flag,
   f.objectId AS objectid,
   f.entry_date_tm                               AS modified,
   dbo.f_map_username(f.user_name)               AS modifiedby,
-  tc.created                                    AS created,
-  tc.createdby                                  AS createdby,
+  COALESCE(tc.created, f.entry_date_tm )  AS created,
+  COALESCE(tc.createdby, dbo.f_map_username(f.user_name))                AS createdby,
   CAST(f.timestamp AS TIMESTAMP) AS timestamp
 
 FROM labkey_etl.f_get_location_with_cage_position() AS f
@@ -50,4 +40,4 @@ FROM labkey_etl.f_get_location_with_cage_position() AS f
 
 GO
 
-grant SELECT on labkey_etl.V_HOUSING to z_labkey
+
