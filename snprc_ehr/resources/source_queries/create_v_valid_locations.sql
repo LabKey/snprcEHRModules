2c3664c85691b4928feae51f44abc528dc25eabe
Scott Rouse <srouse@txbiomed.org>
2018-03-21 14:25:12 +0000

    snprc - added snprc close_date as dateDisabled to valid_locations ETL

--- snprc_ehr/resources/source_queries/create_v_valid_locations.sql
+++ snprc_ehr/resources/source_queries/create_v_valid_locations.sql
@@ -1,5 +1,5 @@
 
-CREATE VIEW [labkey_etl].[v_valid_locations]
+ALTER VIEW [labkey_etl].[v_valid_locations]
 AS
 -- ==========================================================================================
 -- Author:		Scott Rouse
@@ -8,26 +8,33 @@ AS
 -- Changes:
 -- removed objectId not LK table
 -- Added Left trim to description to cap at 100 char
+-- 03.21.2018
+-- Added close_date as dateDisabled.
+--	Because SNPRC locations are date bound, has the potential for multiple rows for a single location.
+--    None currently exists.  Not addressing at this time. srr
+
 --
 -- Note:  Had to add alt Key to XML.  Do not know why. srr
+
 -- ==========================================================================================
 
-  SELECT cast(vl.location as varchar(100) )AS room,
-    LEFT(vl.description,100) AS [building],
-    CAST(vl.location AS INT) AS [area],
-    vl.max_num_cages AS [maxCages],
-    vl.location * 100 AS sort_order,
-   -- vl.object_id AS [objectid],
-    vl.entry_date_tm AS [modified],
-    dbo.f_map_username(vl.user_name) AS [modifiedby],
-    tc.created                       AS created,
-    tc.createdby                     AS createdby,
-    vl.timestamp
+SELECT cast(vl.location as varchar(100) )AS room,
+  LEFT(vl.description,100) AS [building],
+  CAST(vl.location AS INT) AS [area],
+  vl.max_num_cages AS [maxCages],
+  vl.location * 100 AS [sort_order],
+  vl.close_date AS [dateDisabled],
+  -- vl.object_id AS [objectid],
+  vl.entry_date_tm AS [modified],
+  dbo.f_map_username(vl.user_name) AS [modifiedby],
+       tc.created                       AS created,
+       tc.createdby                     AS createdby,
+  vl.timestamp
 
-  FROM animal.dbo.valid_locations vl
-    INNER JOIN dbo.TAC_COLUMNS AS tc ON tc.object_id = vl.object_id;
+FROM animal.dbo.valid_locations vl
+  INNER JOIN dbo.TAC_COLUMNS AS tc ON tc.object_id = vl.object_id;
 
 go
 
-  GRANT SELECT ON labkey_etl.v_valid_locations TO z_labkey;
+GRANT SELECT ON labkey_etl.v_valid_locations TO z_labkey;
 GO
