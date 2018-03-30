3a6f95e04d1cd22fbac53911b262dc30bf257670
Terry Hawkins <thawkins@txbiomed.org>
2018-03-30 17:40:49 +0000

    snprc - removed unnecessary columns from ETL source query.

--- snprc_ehr/resources/source_queries/create_v_snd_attributeData.sql
+++ snprc_ehr/resources/source_queries/create_v_snd_attributeData.sql
@@ -1,4 +1,4 @@
-CREATE VIEW [labkey_etl].[v_snd_attributeData]
+ALTER VIEW [labkey_etl].[v_snd_attributeData]
 AS
 -- ==========================================================================================
 -- Author:		Terry Hawkins
@@ -15,10 +15,6 @@ SELECT
   cp.PROC_ID                       AS EventDataId,
   sp.SUPER_PKG_ID                  AS SuperPkgId,
   pbi.SUPER_PKG_ID                 AS ParentSuperPkgId,
-  cp.ENTRY_DATE_TM                 AS modified,
-  dbo.f_map_username(cp.USER_NAME) AS modifiedby,
-  tc.created                       AS created,
-  tc.createdby                     AS createdby,
 
   -- exp.ObjectProperty columns
   cpa.ATTRIB_KEY AS [ KEY],
@@ -45,8 +41,6 @@ INNER JOIN dbo.SUPER_PKGS AS sp ON sp.SUPER_PKG_ID = bi.SUPER_PKG_ID
 INNER JOIN dbo.PKGS AS p ON p.PKG_ID = sp.PKG_ID
 INNER JOIN dbo.PKG_ATTRIBS AS pa ON pa.PKG_ID = p.PKG_ID AND pa.ATTRIB_KEY = cpa.ATTRIB_KEY
 
--- get created and createdBy columns for coded_procs
-LEFT OUTER JOIN dbo.TAC_COLUMNS AS tc ON tc.object_id = cp.OBJECT_ID
 
 -- select primates only from the TxBiomed colony
 INNER JOIN labkey_etl.V_DEMOGRAPHICS AS D ON D.id = ae.ANIMAL_ID;
