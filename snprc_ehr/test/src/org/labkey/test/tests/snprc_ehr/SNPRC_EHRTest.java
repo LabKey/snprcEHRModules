74f21052a29d5becb8ae63f0a87afacada004c17
Marty Pradere <martyp@labkey.com>
2018-05-09 23:26:26 +0000

    Fix test, updated data region title.

--- snprc_ehr/test/src/org/labkey/test/tests/snprc_ehr/SNPRC_EHRTest.java
+++ snprc_ehr/test/src/org/labkey/test/tests/snprc_ehr/SNPRC_EHRTest.java
@@ -870,7 +870,7 @@ public class SNPRC_EHRTest extends AbstractGenericEHRTest implements SqlserverOn
         ColonyOverviewPage overviewPage = ColonyOverviewPage.beginAt(this, getProjectName());
         ColonyOverviewPage.BaboonColonyTab baboonColonyTab = overviewPage.clickBaboonColonyTab();
         String[] censusColumns = {"Investigator", "Protocol", "M", "F", "SPF", "Conventional", "Total"};
-        List<String> row = baboonColonyTab.getAssignedFundedDataRegion().getRows(censusColumns).get(0);
+        List<String> row = baboonColonyTab.getActiveIacucDataRegion().getRows(censusColumns).get(0);
         List<String> expectedRows =
                 Arrays.asList("dummyinvestigator", "dummyprotocol", "2", "2", " ", "1", "4");
         assertEquals(expectedRows, row);
