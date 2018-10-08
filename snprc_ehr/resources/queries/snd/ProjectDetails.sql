08fb75567ce2e68ddeb76310abad61f3051b9189
Terry Hawkins <thawkins@txbiomed.org>
2018-10-08 17:25:18 +0000

    snprc - tweaked query columns

--- snprc_ehr/resources/queries/snd/ProjectDetails.sql
+++ snprc_ehr/resources/queries/snd/ProjectDetails.sql
@@ -7,5 +7,11 @@ SELECT p.ProjectId,
        p.ProjectType,
        p.ReferenceId.project as ChargeId,
        p.description as Description,
-       p.ReferenceId.protocol as Iacuc
+       p.ReferenceId.protocol as Iacuc,
+       p.ReferenceId.protocol.veterinarian as veterinarian
 from snd.Projects as p
+where p.ReferenceId < 4000 and p.ReferenceId > 0
+and p.Active = true
+and p.ReferenceId.protocol is not null
+and now() between p.StartDate and coalesce(p.EndDate, now())
+
