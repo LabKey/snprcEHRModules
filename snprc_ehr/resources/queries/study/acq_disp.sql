d7ce36fde15e6f1e1d483a8d6bd0b4127c7d4ab9
Scott Rouse <srouse@txbiomed.org>
2018-11-08 20:53:15 +0000

    snprc  updated acq_disp query

--- snprc_ehr/resources/queries/study/acq_disp.sql
+++ snprc_ehr/resources/queries/study/acq_disp.sql
@@ -14,11 +14,19 @@
  * limitations under the License.
  */
 -- working code to emulate acq_disp
--- 8/31/2016 Scott Rouse
-
+-- 08/31/2016 Scott Rouse
+-- Added codes to xml so acq and disp codes (int)  will be in result set
+-- 04/03/2018 Scott Rouse
+-- Cleaned up
+-- 11/08/2018
 SELECT
-a.id as id, a.date as acq_date,a.AcquisitionType.value as acq_code, a.acquisitionType,
-            d.date as disp_date, d.dispositionType.value as disp_code, d.dispositionType
+a.id as id,
+a.date as acq_date,
+a.AcquisitionType.value as acq_code,
+a.acquisitionType,
+d.date as disp_date,
+d.dispositionType.value as disp_code,
+d.dispositionType
 FROM arrival a
 	LEFT OUTER JOIN departure d
 	on d.id = a.id and a.date < ifnull(d.date,curdate())
