cf984db7cc62cae830487d64ace3baf807d49c07
labkey-eyounske <eyounske@labkey.com>
2018-11-12 16:50:01 -0800

    Copyright and license updates   -- Update for November 2018   -- Now scanning .tf and .tfvars files (Terraform)   -- Deliberately leaving out SNPRC module (moving to Git very soon)

--- webapp/snd/test/deepmerge.js
+++ webapp/snd/test/deepmerge.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 
 // Written by David Walsh, https://davidwalsh.name/javascript-deep-merge
 function isMergeableObject(val) {
