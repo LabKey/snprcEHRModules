fc30a9f0e28fc4c7b5cd6d42c2ea2f90b97fbe05
labkey-eyounske <eyounske@labkey.com>
2018-11-14 15:15:24 -0800

    Copyright and license updates   -- Update copyright for November 2018, round 4

--- snprc_ehr/resources/source_queries/create_v_animal_ownership.sql
+++ snprc_ehr/resources/source_queries/create_v_animal_ownership.sql
@@ -1,6 +1,6 @@
 ALTER VIEW [labkey_etl].[V_ANIMAL_OWNERSHIP] AS
 /*
- * Copyright (c) 2015-2016 LabKey Corporation
+ * Copyright (c) 2016-2018 LabKey Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
