75a7b32f569ff3e29c93a29f5ee2abfc81f566c2
labkey-ankurj <39965420+labkey-ankurj@users.noreply.github.com>
2019-06-17 14:06:34 -0700

    Item 5760: update copyrights to 2019

--- snprc_ehr/resources/schemas/dbscripts/sqlserver/snprc_ehr-19.11-19.12.sql
+++ snprc_ehr/resources/schemas/dbscripts/sqlserver/snprc_ehr-19.11-19.12.sql
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2019 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 -- Version 12 is replaced by version 13 srr
 
 EXEC core.fn_dropifexists 'ValidDiet','snprc_ehr', 'TABLE';
