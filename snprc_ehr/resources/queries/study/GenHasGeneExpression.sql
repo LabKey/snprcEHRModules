39b8463e2120bc28f5c115db3e77c262c2b4af78
Scott Rouse <GitKraken@srrouse.com>
2019-09-20 14:27:52 -0500

    snprc srr missed ETL file

---
+++ snprc_ehr/resources/queries/study/GenHasGeneExpression.sql
@@ -0,0 +1,5 @@
+select distinct g.subjectId as Id,
+                TRUE  as HasGeneExpressionData,
+                now() as date,
+                NULL as objectId
+from Project."Core Facilities/Genetics".assay.general."Gene Expression".data g
