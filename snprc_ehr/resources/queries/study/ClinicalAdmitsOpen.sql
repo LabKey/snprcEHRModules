ce173b960538c3edabeab083e28bc7beb13cd4fa
Scott Rouse <GitKraken@srrouse.com>
2019-11-14 10:18:19 -0600

    SNPRC srr updated queries

--- snprc_ehr/resources/queries/study/ClinicalAdmitsOpen.sql
+++ snprc_ehr/resources/queries/study/ClinicalAdmitsOpen.sql
@@ -4,11 +4,12 @@ For SNPRC mobile app
 
 
 srr 10.29.2019
+srr 11.14.19 Added alias for species and added CaseId
 ********************************************************************/
 
-SELECT c.Id, c.date AS AdmitDate, c.enddate AS ReleaseDate,
+SELECT c.Id, c.date AS AdmitDate, c.enddate AS ReleaseDate, c.caseid as CaseId,
        c.category, c.problem AS PDX, c.admitcomplaint, c.resolution, c.objectid, c.assignedvet, c.assignedvet.displayName as VetName,
-       c.Id.Demographics.species.arc_species_code
+       c.Id.Demographics.species.arc_species_code as Species
 FROM study.cases c
 WHERE c.enddate IS NULL
 --limit 30
