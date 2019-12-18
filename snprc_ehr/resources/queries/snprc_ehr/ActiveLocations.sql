a22edd362504004cc8d0f268ccc7bcfbcd65f701
srouseTxbiomed <srouse@txbiomed.org>
2019-12-18 11:01:13 -0600

    SNPRC srr location queries (#172)
    Adds ActiveLocationsAll

--- snprc_ehr/resources/queries/snprc_ehr/ActiveLocations.sql
+++ snprc_ehr/resources/queries/snprc_ehr/ActiveLocations.sql
@@ -5,6 +5,9 @@ Returns Species (char2) and room
 for all locations that currently hold animals
   srr 07.10.2019
 
+  This could be refactored out by restricting
+    ActiveLocationsAll.sql to where species is NOT NULL
+  srr 12.10.2019
 ***************************************/
 SELECT distinct d.species.arc_species_code as species,d2.room
 
