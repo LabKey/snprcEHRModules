ed2ca96e3cc1c451bf103ade1d34acd585a95ba5
Spamhurts <thawkins@txbiomed.org>
2020-01-25 12:45:47 -0600

    Added sort column to ActiveLocations.sql

--- snprc_ehr/resources/queries/snprc_ehr/ActiveLocations.sql
+++ snprc_ehr/resources/queries/snprc_ehr/ActiveLocations.sql
@@ -9,11 +9,11 @@ for all locations that currently hold animals
     ActiveLocationsAll.sql to where species is NOT NULL
   srr 12.10.2019
 ***************************************/
-SELECT distinct d.species.arc_species_code as species,d2.room
+SELECT distinct d.species.arc_species_code as species,d2.room, 	cast(d2.room_sortValue as double) as room_sortValue
 
 FROM study.housing d2
          inner join study.demographics d
                     on d2.id = d.id
 WHERE d2.enddate IS NULL
   AND d2.qcstate.publicdata = true
-ORDER by d2.room;
+ORDER by cast(d2.room_sortValue as double);
