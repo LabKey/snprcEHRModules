d457b214199cfd00f60c9c6a898800c15ada2008
srouseTxbiomed <srouse@txbiomed.org>
2020-02-29 08:57:09 -0600

    Dev back to NewAnimal (#193)
    * Added remark column to miscPivot.sql - SqlMed is recording results in the remarks field (#191)
    
    * 1. Added species column index to study.demographics (#190)
    
    2. Tweaked species query to exclude animal species that are no longer in the colony
    
    Co-authored-by: spamhurts <thawkins@txbiomed.org>

--- snprc_scheduler/resources/queries/snd/SpeciesCodesActiveProjects.sql
+++ snprc_scheduler/resources/queries/snd/SpeciesCodesActiveProjects.sql
@@ -8,3 +8,4 @@ from snprc_ehr.species s
          inner join snd.Projects p
                  on p.referenceID = ps.project
 where  p.Active = True and s.primate  ='Y'
+  and exists (select 1 from study.demographics as d where d.calculated_status = 'Alive' and species = s.species_code)
