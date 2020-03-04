c7704c573db2a8d1906c12becacf2dd8edc2ad8e
Spamhurts <thawkins@txbiomed.org>
2020-03-04 15:58:20 -0600

    1. Overrode the meta data for study.encounters remarks column - was in shared config - moved to table config - column type set to nvarchar(max) 2. Tweaked ETL settings for flag_values.xml 3. fixed bug in source query table: create_v_valid_dx_list.sql

--- snprc_ehr/resources/source_queries/create_v_valid_dx_list.sql
+++ snprc_ehr/resources/source_queries/create_v_valid_dx_list.sql
@@ -5,7 +5,7 @@
  **************************************************/
 
 
-CREATE VIEW [labkey_etl].[v_valid_dx_list] AS
+ALTER VIEW [labkey_etl].[v_valid_dx_list] AS
 -- ====================================================================================================================
 -- Object: v_valid_dx_list
 -- Author:		Scott Rouse
@@ -16,16 +16,17 @@ CREATE VIEW [labkey_etl].[v_valid_dx_list] AS
 SELECT vd.dx_group                      AS DXGroup,
        vd.dx                            AS DX,
        vd.dx_status                     AS DXStatus,
-       vd.object_id                     AS objectid,
+       vd.object_id                     AS ObjectId,
        vd.entry_date_tm                 AS modified,
        dbo.f_map_username(vd.user_name) AS modifiedby,
-       tc.created                       AS created,
-       tc.createdby                     AS createdby,
-       timestamp
+	   COALESCE(tc.created, vd.entry_date_tm )  AS created,
+	   COALESCE(tc.createdby, dbo.f_map_username(vd.user_name)) AS createdby,
+       vd.timestamp
 FROM dbo.valid_dx_list vd
-         LEFT OUTER JOIN dbo.TAC_COLUMNS AS tc
-                         ON tc.object_id = vd.object_id
-    GO
+LEFT OUTER JOIN dbo.TAC_COLUMNS AS tc ON tc.object_id = vd.object_id
+ 
+GO
 
 GRANT SELECT ON labkey_etl.v_valid_dx_list TO z_labkey
-    GO
+
+GO
