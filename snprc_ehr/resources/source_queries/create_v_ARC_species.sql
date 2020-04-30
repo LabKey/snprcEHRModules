a72be06275e02995716305b743c07ff6d182c597
srouseTxbiomed <srouse@txbiomed.org>
2020-04-30 11:52:42 -0500

    Fb snprc arc species etl (#227)
    * snprc ehr_lookups species_code ETL
    
    snprc ARC species code (char 2) etl
    
    * snprc ehr_lookups species_code ETL type
    
    fixed typo
    snprc ARC species code (char 2) etl
    
    * snprc ehr_lookups species_code ETL type
    
    dropped v_delete_valid_ARC_species
    
    snprc ARC species code (char 2) etl
    
    * snprc ehr_lookups species_code ETL type
    
    removed species TSV
    
    snprc ARC species code (char 2) etl
    
    * snprc ehr_lookups species_code ETL type
    
    removed species TSV
    
    snprc ARC species code (char 2) etl

---
+++ snprc_ehr/resources/source_queries/create_v_ARC_species.sql
@@ -0,0 +1,30 @@
+/*==============================================================*/
+/* View: v_valid_ARC_Species                                    */
+/*==============================================================*/
+CREATE VIEW labkey_etl.v_ARC_Species as
+    -- ====================================================================================================================
+-- Object: v_valid_ARC_Species
+-- Author:		Scott Rouse
+-- Create date: 04/28/2020
+-- 04/29/20
+-- corrected typo
+-- Note:
+--
+-- ========================================================================== ================
+
+SELECT a.arc_species_code                                      AS code,
+       a.common_name                                           AS common_name,
+       a.scientific_name                                       AS scientific_name,
+       a.primate                                               AS PrimateFlag,
+       a.entry_date_tm                                         AS modified,
+       dbo.f_map_username(a.user_name)                         AS modifiedby,
+       COALESCE(tc.created, a.entry_date_tm)                 AS created,
+       COALESCE(tc.createdby, dbo.f_map_username(a.user_name)) AS createdBy,
+       a.object_id                                             AS objectid,
+       a.timestamp
+FROM animal.dbo.arc_valid_species_codes a
+         LEFT OUTER JOIN dbo.TAC_COLUMNS AS tc ON tc.object_id = a.object_id;
+go
+
+GRANT SELECT ON labkey_etl.v_ARC_Species TO z_labkey;
+go
