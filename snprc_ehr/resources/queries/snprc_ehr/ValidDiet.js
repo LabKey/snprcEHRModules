1cb86291e5820cadcc2a856e6552382df4767772
srouseTxbiomed <srouse@txbiomed.org>
2020-05-21 13:59:13 -0500

    SNPRC srr Diet changes for terry

--- snprc_ehr/resources/queries/snprc_ehr/ValidDiet.js
+++ snprc_ehr/resources/queries/snprc_ehr/ValidDiet.js
@@ -1,12 +1,10 @@
-var console = require("console");
+//var console = require("console");
 var LABKEY = require("labkey");
 var snprcTriggerHelper = new org.labkey.snprc_ehr.query.SNPRC_EHRTriggerHelper(LABKEY.Security.currentUser.id, LABKEY.Security.currentContainer.id);
-console.log("Started");
 function beforeInsert(row,errors) {
-    //console.log("Inside beforeInsert");
     var dCode = snprcTriggerHelper.getNextDietCode();
-    console.log("dCode is " + dCode);
-    console.log("row is " + row);
+    //console.log("dCode is " + dCode);
+    //console.log("row is " + row);
     if (row.DietCode === undefined || row.DietCode == 'undefined') {
        row.DietCode = dCode;
       //  row.DietCode  = snprcTriggerHelper.getNextDietCode();
@@ -18,8 +16,4 @@ function beforeInsert(row,errors) {
     }
     row.objectid = row.objectid || LABKEY.Utils.generateUUID().toUpperCase();
 
-    console.log("objectid: " + row.objectid);
-    console.log("DietCode: " + row.DietCode);
-    console.log("snom: " + row.SnomedCode);
-    console.log("Ending row is " + row);
 }
