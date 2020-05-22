cc987971a402efc17801b0cb62367c252e76e6b8
srouseTxbiomed <srouse@txbiomed.org>
2020-05-22 12:21:39 -0500

    Fb snprc diet redo (#235)
    * DietReDo2  Code that was not pushed srr 05.21.20
    
    * SNPRC srr Diet changes for terry

--- snprc_ehr/resources/queries/snprc_ehr/ValidDiet.js
+++ snprc_ehr/resources/queries/snprc_ehr/ValidDiet.js
@@ -1,19 +1,15 @@
-/*
- * Copyright (c) 2019 LabKey Corporation
- *
- * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
- */
-var console = require("console");
+//var console = require("console");
 var LABKEY = require("labkey");
 var snprcTriggerHelper = new org.labkey.snprc_ehr.query.SNPRC_EHRTriggerHelper(LABKEY.Security.currentUser.id, LABKEY.Security.currentContainer.id);
-// Generates SNPRC "Snomedcode" for diet srr 03.21.2019
 function beforeInsert(row,errors) {
-    //console.log("Inside beforeInsert");
     var dCode = snprcTriggerHelper.getNextDietCode();
-
+    //console.log("dCode is " + dCode);
+    //console.log("row is " + row);
     if (row.DietCode === undefined || row.DietCode == 'undefined') {
        row.DietCode = dCode;
-        // Refactored to generate FauxMed code if DietCode is generated srr 03.26.19
+      //  row.DietCode  = snprcTriggerHelper.getNextDietCode();
+    }
+    if (row.SnomedCode === undefined || row.SnomedCode == 'undefined') {
         // Used to generate a 7 character FauxMed code i.e. S-001234
         var fauxRight = "00000" + dCode;
         row.SnomedCode = "S-" + fauxRight.slice(fauxRight.length-5,fauxRight.length);
