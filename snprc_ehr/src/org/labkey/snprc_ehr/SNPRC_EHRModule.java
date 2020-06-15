5f3421a3314c225595546959bb2aa8c73b810ce2
srouseTxbiomed <srouse@txbiomed.org>
2020-06-15 11:24:33 -0500

    Fb snprc new animal update (#245)
    * snprc NewAnimalData table update
    
    Added missed column to NewAnimalData
    
    * NewAnimalUpdate FK to Birthcodes

--- snprc_ehr/src/org/labkey/snprc_ehr/SNPRC_EHRModule.java
+++ snprc_ehr/src/org/labkey/snprc_ehr/SNPRC_EHRModule.java
@@ -117,7 +117,7 @@ public class SNPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 20.006;
+        return 20.007;
     }
 
     @Override
