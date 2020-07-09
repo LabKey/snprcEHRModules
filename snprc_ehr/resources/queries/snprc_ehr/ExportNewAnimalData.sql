bb43d53014aaa610690dd8d25ad37ad5455a65cb
srouseTxbiomed <srouse@txbiomed.org>
2020-07-09 11:09:41 -0500

    snprc ExportNewAnimalData to CAMP (#260)
    * snprc ExportNewAnimalData to CAMP
    07.09.2020
    
    * snprc ExportNewAnimalData to CAMP
    Cage position added
    07.09.2020

---
+++ snprc_ehr/resources/queries/snprc_ehr/ExportNewAnimalData.sql
@@ -0,0 +1,44 @@
+/********************************************************
+  Query will feed the export ETL back to CAMP.
+      CampNewAnimalData
+
+
+ srr 07.07.2020
+ ********************************************************/
+
+SELECT
+    n.Id,
+    n.BirthDate,
+    n.BirthCode as Bd_Status,
+    n.AcquisitionType,
+    n.AcqDate AS AcquisitionDate,
+    n.species,
+    n.Gender,
+    n.sire,
+    n.dam,
+    n.Diet as DietInt,
+    n.Diet.diet AS DietText,
+    --CAST(diet AS INTEGER) AS DietTid,
+    n.pedigree.name AS PedigreeText,
+    n.pedigree AS PedigreeInt,
+    --n.Colony AS ColonyInt, -- not in valid_colony
+    n.Colony.Name AS ColonyText,
+    n.AnimalAccount,
+    --n.AnimalAccount.Account AS AccountText,
+    --CAST(n.AnimalAccount AS INTEGER) AnimalAccountInt,
+    --n.room, -- not in valid_locations
+    n.IACUC,
+    n.room.room,
+    n.cage,
+    n.OwnerInstitution,
+    n.ResponsibleInstitution,
+    n.Objectid,
+    n.Modified,
+    n.ModifiedBy,
+    n.ModifiedBy.email AS ModifiedByEmail,
+    n.Created,
+    n.CreatedBy,
+    n.CreatedBy.email AS CreatedByEmail,
+    n.CreatedBy.displayname
+
+FROM snprc_ehr.NewAnimalData n
