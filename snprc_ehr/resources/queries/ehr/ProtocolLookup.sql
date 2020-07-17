e11afbd7398bcd2acf9acee26cd555d481c20f5b
Terry J Hawkins <thawkins@txbiomed.org>
2020-07-17 15:49:42 -0500

    Fb new animal wizard dev (#267)
    * Fixed typo
    
    * New Animal Wizard dev
    1. updated sort in fetchProtocols.js
    2. updated protocolLookup.sql query
    3. don't reset dam, sire, and cage after save for marmoset births
    4. verify user's group membership before displaying New Animal Wizard link on snprc_ehr landing page
       (minor refactor of begin.html)
    5. changed text color on information panel

--- snprc_ehr/resources/queries/ehr/ProtocolLookup.sql
+++ snprc_ehr/resources/queries/ehr/ProtocolLookup.sql
@@ -1,6 +1,8 @@
 SELECT p.protocol AS Iacuc,
        p.protocol + ' - ' +  RTRIM(p.inves)+ ' -' + RTRIM(p.title) AS DisplayValue,
        RIGHT(p.protocol, 2) AS Species,
+       p.project_type as ProjectType,
+       p.sequenceNumber as SequenceNumber,
        p.approve AS ApprovalDate,
        p.enddate AS EndDate,
        p.maxAnimals AS MaxAnimals
