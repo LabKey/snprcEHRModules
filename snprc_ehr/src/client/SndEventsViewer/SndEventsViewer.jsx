b4a44c5e566a558ed9922ed948f29e7752f32a8e
Spamhurts <thawkins@txbiomed.org>
2020-08-29 11:46:08 -0500

    Router refactor

--- snprc_ehr/src/client/SndEventsViewer/SndEventsViewer.jsx
+++ snprc_ehr/src/client/SndEventsViewer/SndEventsViewer.jsx
@@ -6,7 +6,7 @@ import './styles/sndEventsViewer.scss'
 import SndEventState from './constants/sndEventsState'
 import constants from './constants/index'
 import SndEventsPanel from './components/SndEventsPanel'
-import { createMemoryHistory, Route, Router, WithRouterProps } from 'react-router'
+import { Route, Router, createMemoryHistory } from 'react-router'
 
 export default class SndEventsViewer extends React.Component {
     state = new SndEventState();
@@ -44,44 +44,6 @@ export default class SndEventsViewer extends React.Component {
         ))
     }
 
-    // Cancel button callback
-    handleCancel = () => {
-        if (this.state.isDirty) {
-            this.setState(prevState => (
-                {
-                    ...prevState,
-                    showCancelModal: true
-                }
-            ))
-        } else {
-            window.history.back()
-        }
-    }
-
-    // reset app
-    onCancelClick = () => {
-        this.setState(prevState => (
-            {
-                ...prevState,
-                isDirty: false,
-                showCancelModal: false
-            }
-        ))
-        window.history.back()
-    }
-
-    // dismiss modals
-    onCloseClick = () => {
-        this.setState(prevState => (
-            {
-                ...prevState,
-                showCancelModal: false,
-                showSaveModal: false,
-                showSpeciesChangeModal: false
-            }
-        ))
-    }
-
     render() {
         // allow debug mode to be triggered for running test suite
         this.debug = this.props.debug !== undefined ? this.props.debug : constants.debug
@@ -93,10 +55,9 @@ export default class SndEventsViewer extends React.Component {
                 <LoadingSpinner msg="Loading page..." />
             )
         }
-        const history = createMemoryHistory()
 
         return (
-            <Router history={ history }>
+            <Router history={createMemoryHistory()}>
                 <div className="parent-panel">
                     <Route path="/" component={ SndEventsPanel } />
                 </div>
