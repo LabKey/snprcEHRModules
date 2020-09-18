ca5df6906ce2b57a6ef4abb0c6d61d6ee9362ef5
srouseTxbiomed <srouse@txbiomed.org>
2020-09-18 12:19:28 -0500

    NAD_Validation (#305)
    09.18.2020
    Check Acquisition type and institutions

--- snprc_ehr/src/org/labkey/snprc_ehr/services/SNPRC_EHRValidator.java
+++ snprc_ehr/src/org/labkey/snprc_ehr/services/SNPRC_EHRValidator.java
@@ -30,8 +30,11 @@ public class SNPRC_EHRValidator
         Map<String, Object> row;
         UserSchema schema = QueryService.get().getUserSchema(u, c, "snprc_ehr");
         UserSchema schemaEHR = QueryService.get().getUserSchema(u, c,"EHR");
-
+        UserSchema schemaEHRLU = QueryService.get().getUserSchema(u, c,"ehr_lookups");
         // species and pedigree
+
+
+
         try
         {
             if (newAnimalData.getSpecies() == null)
@@ -53,18 +56,34 @@ public class SNPRC_EHRValidator
 
         }
 
-        if (newAnimalData.getBirthCode() == null)
-            throw new ValidationException("Birthdate status is required");
+            if (newAnimalData.getBirthCode() == null)
+                throw new ValidationException("Birthdate status is required");
 
+        // Acquisition Code-AcquisitionType
+        try
+        {
+            if (newAnimalData.getAcquisitionType() == null)
+                throw  new ValidationException("Valid Acquisition code is required");
+
+            TableInfo ti = schemaEHRLU.getTable("AcquisitionType", schema.getDefaultContainerFilter());
+            SimpleFilter filter = new SimpleFilter(FieldKey.fromParts("value"), newAnimalData.getAcquisitionType().toString(), CompareType.EQUAL);
+            TableSelector ts = new TableSelector(ti, filter, null);
+
+            if (ts.getRowCount() != 1 )
+                throw new ValidationException("Acquisition code is required");
+
+
+        }
+        catch (Exception e)
+        {
+            throw new ValidationException(e.getMessage());
+        }
 
         // bdstatus
         if (newAnimalData.getBirthCode() == null)
             throw new ValidationException("Birth code is required");
 
 
-        // acquisitionType
-        if (newAnimalData.getAcquisitionType() == null)
-            throw new ValidationException("Acquisition type is required");
         // gender
         if (newAnimalData.getGender() == null)
             throw new ValidationException("Gender is required");
@@ -91,12 +110,38 @@ public class SNPRC_EHRValidator
         {
             throw new ValidationException(e.getMessage());
         }
-        // ownerInstitution
-        if (newAnimalData.getOwnerInstitution() == null)
-            throw new ValidationException("Owner Institution is required");
-        //responsibleInstitution
-        if (newAnimalData.getResponsibleInstitution() == null)
-            throw new ValidationException("Responsible Institution is required");
+
+        // Valid Institutions
+        try
+        {
+            // ownerInstitution
+            if (newAnimalData.getOwnerInstitution() == null)
+                throw new ValidationException("Owner Institution is required");
+            //responsibleInstitution
+            if (newAnimalData.getResponsibleInstitution() == null)
+                throw new ValidationException("Responsible Institution is required");
+
+            TableInfo ti = schema.getTable("validInstitutions", schema.getDefaultContainerFilter());
+            SimpleFilter filterRI = new SimpleFilter(FieldKey.fromParts("institution_id"), newAnimalData.getResponsibleInstitution(), CompareType.EQUAL);
+
+            TableSelector tsRI = new TableSelector(ti, filterRI, null);
+            if (tsRI.getRowCount() != 1 )
+                throw new ValidationException("Responsible Institution is required");
+
+            SimpleFilter filterOI = new SimpleFilter(FieldKey.fromParts("institution_id"), newAnimalData.getOwnerInstitution(), CompareType.EQUAL);
+
+            TableSelector tsOI = new TableSelector(ti, filterOI, null);
+            if (tsOI.getRowCount() != 1 )
+                throw new ValidationException("Responsible Institution is required");
+
+
+
+
+        }
+        catch (Exception e)
+        {
+            throw new ValidationException(e.getMessage());
+        }
         // room
         if (newAnimalData.getRoom() == null)
             throw new ValidationException("Room is required");
