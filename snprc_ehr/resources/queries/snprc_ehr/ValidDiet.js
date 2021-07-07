58635d51b559cec9cd83503c024fd957ae65befa
srouseTxbiomed <srouse@txbiomed.org>
2021-07-07 14:28:41 -0500

    ValidDiet.js was using wrong value to generate snomed code create_v_valid_diet.sql Cleaned up formatting. ExportNewAnimalData.sql now removes seconds from date-time values
    removed console logs
    srr 07.07.2021

--- snprc_ehr/resources/queries/snprc_ehr/ValidDiet.js
+++ snprc_ehr/resources/queries/snprc_ehr/ValidDiet.js
@@ -4,18 +4,14 @@ var LABKEY = require("labkey");
 var snprcTriggerHelper = new org.labkey.snprc_ehr.query.SNPRC_EHRTriggerHelper(LABKEY.Security.currentUser.id, LABKEY.Security.currentContainer.id);
 
 function beforeInsert(row,errors) {
+    // TAC generated diet only
     var dCode = snprcTriggerHelper.getNextDietCode();
-    console.log("dCode is " + dCode);
-    console.log("row is " + row);
-    console.log("rowDietCodePre is " + row.DietCode);
 
     if (row.DietCode === undefined || row.DietCode == 'undefined') {
        row.DietCode = dCode;
       //  row.DietCode  = snprcTriggerHelper.getNextDietCode();
     }
 
-    console.log("rowDietCodePost is " + row.DietCode);
-    console.log("rowSnomedCodePre is " + row.SnomedCode);
     if (row.SnomedCode === undefined || row.SnomedCode == 'undefined') {
         // Used to generate a 7 character FauxMed code i.e. S-001234
 
@@ -24,7 +20,6 @@ function beforeInsert(row,errors) {
         var fauxRight = "00000" + row.DietCode;
         row.SnomedCode = "S-" + fauxRight.slice(fauxRight.length-5,fauxRight.length);
     }
-    console.log("rowSnomedCodePost is " + row.SnomedCode);
     row.objectid = row.objectid || LABKEY.Utils.generateUUID().toUpperCase();
 
 }
