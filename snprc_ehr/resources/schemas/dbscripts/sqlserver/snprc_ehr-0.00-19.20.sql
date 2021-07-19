f427e32f7a72e812bf551ed552bf2f83d27b6666
Adam Rauch <adam@labkey.com>
2021-07-19 14:22:51 -0700

    Switch earliest upgrade version to 19.20 and consolidate all earlier scripts to 0.00-19.20 (#375)

--- snprc_ehr/resources/schemas/dbscripts/sqlserver/snprc_ehr-0.00-19.20.sql
+++ snprc_ehr/resources/schemas/dbscripts/sqlserver/snprc_ehr-0.00-19.20.sql
@@ -872,3 +872,137 @@ CREATE TABLE [snprc_ehr].[LocationTemperature](
 CREATE UNIQUE INDEX idx_snprc_LocationTemperature_objectid ON snprc_ehr.LocationTemperature (ObjectId);
 CREATE UNIQUE INDEX idx_snprc_LocationTemperature_Room ON snprc_ehr.LocationTemperature (Room);
 GO
+
+EXEC core.fn_dropifexists 'LocationTemperature','snprc_ehr', 'TABLE';
+
+GO
+/*************************************
+ObjectId should be populated before insert.
+
+
+srr 02.25.2019 ori
+srr 02.27.2019 19.11 version
+*************************************/
+
+CREATE TABLE [snprc_ehr].[LocationTemperature](
+  [Room] [varchar](100) NOT NULL,
+  [Date] [DATETIME] NOT NULL,
+  [LowTemperature] [NUMERIC](6, 2) NULL,
+  [HighTemperature] [NUMERIC](6, 2) NULL,
+  [Notify] [VARCHAR](18) NULL,
+  [Created] [datetime] NULL,
+  [CreatedBy] [dbo].[USERID] NULL,
+  [Modified] [datetime] NULL,
+  [ModifiedBy] [dbo].[USERID] NULL,
+  [diCreated] [datetime] NULL,
+  [diModified] [datetime] NULL,
+  [diCreatedBy] [dbo].[USERID] NULL,
+  [diModifiedBy] [dbo].[USERID] NULL,
+  [Container] [dbo].[ENTITYID] NOT NULL,
+  [objectid] [uniqueidentifier] NOT NULL
+
+
+  CONSTRAINT [PK_LocationTemperature] PRIMARY KEY CLUSTERED ([Room] ASC,[Date] ASC)
+  CONSTRAINT FK_snprc_LocationTemperature_container FOREIGN KEY (Container) REFERENCES core.Containers (EntityId)
+
+  )
+  GO
+
+
+CREATE UNIQUE INDEX idx_snprc_LocationTemperature_objectid ON snprc_ehr.LocationTemperature (ObjectId);
+CREATE INDEX idx_snprc_LocationTemperature_Date ON snprc_ehr.LocationTemperature (Date, Room);
+GO
+
+-- Version 12 is replaced by version 13 srr
+
+EXEC core.fn_dropifexists 'ValidDiet','snprc_ehr', 'TABLE';
+
+--GO;
+/***************************************************
+Note:  ArcSpeciesCode is null for most rows
+      therefore not included in PK
+ PK is composite of Diet and StartDate
+
+ Currently: In legacy table, SnomedCode is
+            generated using a identity (tid).
+
+            This was refactored to an integer
+            DietId.
+            May refactor out or to a counter
+             value at a later date.
+
+srr 03.11.2019 ori
+***************************************************/
+
+CREATE TABLE snprc_ehr.ValidDiet(
+  [Diet] [nvarchar](20) NOT NULL,
+  [ArcSpeciesCode] [nvarchar](2) NULL,
+  [StartDate] [datetime] NOT NULL,
+  [StopDate] [datetime] NULL,
+  [SnomedCode] [nvarchar](7) NULL,
+  [DietId]	[INTEGER] NOT NULL,
+  --ObjectId] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+  --[Container] [entityID] NOT NULL,
+  [Created]	DATETIME,
+  [CreatedBy] USERID,
+  [Modified]	DATETIME,
+  [ModifiedBy] USERID,
+  [diCreated] [datetime] NULL,
+  [diModified] [datetime] NULL,
+  [diCreatedBy] [dbo].[USERID] NULL,
+  [diModifiedBy] [dbo].[USERID] NULL,
+  [Container] [dbo].[ENTITYID] NOT NULL,
+  [objectid] [uniqueidentifier] NOT NULL
+  CONSTRAINT [PK_ValidDiet] PRIMARY KEY CLUSTERED ([Diet] ASC,[StartDate] ASC)
+  )
+
+  go
+
+EXEC core.fn_dropifexists 'ValidDiet','snprc_ehr', 'TABLE';
+
+/*****************************************************************
+Note:  ArcSpeciesCode is null for most rows
+therefore not included in PK
+PK is composite of Diet and StartDate
+
+Currently: In legacy table, SnomedCode is
+generated using a identity (tid).
+As of 03.18.2019 column is now DietCode.
+
+This was refactored to an integer
+DietId.
+May refactor out or to a counter
+value at a later date.
+
+srr 03.11.2019 ori
+srr 03.18.2019 To agree w/ naming conventions elsewhere
+refactored DietId to DietCode
+
+
+******************************************************************/
+
+CREATE TABLE snprc_ehr.ValidDiet(
+  [Diet] [nvarchar](20) NOT NULL,
+  [ArcSpeciesCode] [nvarchar](2) NULL,
+  [StartDate] [datetime] NOT NULL,
+  [StopDate] [datetime] NULL,
+  [SnomedCode] [nvarchar](7) NULL,
+  [DietCode]	[INTEGER] NOT NULL,
+  --ObjectId] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+  --[Container] [entityID] NOT NULL,
+  [Created]	DATETIME,
+  [CreatedBy] USERID,
+  [Modified]	DATETIME,
+  [ModifiedBy] USERID,
+  [diCreated] [datetime] NULL,
+  [diModified] [datetime] NULL,
+  [diCreatedBy] [dbo].[USERID] NULL,
+  [diModifiedBy] [dbo].[USERID] NULL,
+  [Container] [dbo].[ENTITYID] NOT NULL,
+  [objectid] [uniqueidentifier] NOT NULL
+  CONSTRAINT [PK_ValidDiet] PRIMARY KEY CLUSTERED ([Diet] ASC,[StartDate] ASC)
+  )
+
+-- will need to be changed if we begin to use Diet instead of SnomedCode srr
+CREATE UNIQUE INDEX idx_ValidDiet_SnomedCode_StartStopDate ON snprc_ehr.ValidDiet(SnomedCode, StartDate, StopDate);
+  go
