11468973d8a971687f8ca3e64356e3b539cb3b3c
Terry J Hawkins <thawkins@txbiomed.org>
2021-08-30 08:43:35 -0500

    Updates for SND lookups editing
    Original PR: https://github.com/LabKey/snd/pull/99

--- resources/queries/snd/Lookups.js
+++ resources/queries/snd/Lookups.js
@@ -1,5 +1,3 @@
-
-
 var console = require("console");
 var LABKEY = require("labkey");
 
@@ -11,7 +9,7 @@ function init(event){
 }
 
 function onUpsert(row, errors) {
-
+    // reuse LookupSetId if the SetName already exists. This primarily happens during ETLs
     if (row.LookupSetId === undefined || row.LookupSetId == 'undefined') {
         LABKEY.Query.selectRows({
             schemaName: 'snd',
@@ -38,6 +36,7 @@ function onUpsert(row, errors) {
 }
 
 function beforeInsert(row, errors) {
+    row.objectId = row.objectId || LABKEY.Utils.generateUUID().toUpperCase();
     onUpsert(row, errors);
 }
 
@@ -45,4 +44,62 @@ function beforeUpdate(row, errors) {
     onUpsert(row, errors);
 }
 
+function beforeDelete(row, errors) {
+/*
+    TODO: Need a better way to determine if a lookupset item is in use. Probably need to add the check to
+     the deleteRows method in LookupsTable.java. NOTE: CAMP allows the lookup item to be deleted if it hasn't
+     been assigned as a default value or used in an event. tjh
+ */
+    if (row.LookupSetId !== undefined) {
+
+        let lookupQuery
+
+        LABKEY.Query.selectRows({
+            schemaName: 'snd',
+            queryName: 'LookupSets',
+            columns: 'SetName',
+            scope: this,
+            filterArray: [
+                LABKEY.Filter.create('LookupSetId', row.LookupSetId, LABKEY.Filter.Types.EQUAL)
+            ],
+            success: function (data) {
+                if (data.rows && data.rows.length) {
+                    lookupQuery = data.rows[0].SetName
+                }
+                else {
+                    errors._form = 'LookupSet not found - shouldn\'t happen'
+                    return
+                }
+
+            },
+            failure: function (error) {
+                console.log('Select rows error');
+                console.log(error);
+            }
+        });
+
+
+        LABKEY.Query.selectRows({
+            schemaName: 'snd',
+            queryName: 'PackageAttribute',
+            columns: 'LookupQuery',
+            scope: this,
+            filterArray: [
+                LABKEY.Filter.create('LookupQuery', lookupQuery, LABKEY.Filter.Types.EQUAL),
+                LABKEY.Filter.create('LookupSchema', 'snd', LABKEY.Filter.Types.EQUAL)
+
+            ],
+            success: function (data) {
+                if (data.rows && data.rows.length) {
+                    errors._form = 'Lookup item cannot be deleted - LookupSet is in use.'
+                    return;
+                }
+            },
+            failure: function (error) {
+                console.log('Select rows error');
+                console.log(error);
+            }
+        });
+    }
+}
 
