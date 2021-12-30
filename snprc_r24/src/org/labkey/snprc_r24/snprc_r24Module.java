effe85402ea6f51db81585d3be2f487c18941571
Adam Rauch <adam@labkey.com>
2021-12-30 13:09:21 -0800

    Bump all schema versions to 22.000

--- snprc_r24/src/org/labkey/snprc_r24/snprc_r24Module.java
+++ snprc_r24/src/org/labkey/snprc_r24/snprc_r24Module.java
@@ -47,7 +47,7 @@ public class snprc_r24Module extends DefaultModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 21.000;
+        return 22.000;
     }
 
     @Override
