47f90494681be8749f3038aa05bb7c9f575ed633
labkey-martyp <martyp@labkey.com>
2022-01-08 07:35:33 -0800

    Disable snprc cases triggers for scheduler testing

--- snprc_scheduler/resources/scripts/snprc_scheduler_triggers.js
+++ snprc_scheduler/resources/scripts/snprc_scheduler_triggers.js
@@ -3,6 +3,9 @@ exports.init = function (EHR) {
     EHR.Server.TriggerManager.registerHandler(EHR.Server.TriggerManager.Events.INIT, function (event, helper, EHR) {
 
         EHR.Server.TriggerManager.unregisterAllHandlersForQueryNameAndEvent('study', 'blood', EHR.Server.TriggerManager.Events.BEFORE_UPSERT);
+        EHR.Server.TriggerManager.unregisterAllHandlersForQueryNameAndEvent('study', 'cases', EHR.Server.TriggerManager.Events.BEFORE_UPSERT);
+        EHR.Server.TriggerManager.unregisterAllHandlersForQueryNameAndEvent('study', 'cases', EHR.Server.TriggerManager.Events.AFTER_DELETE);
+        EHR.Server.TriggerManager.unregisterAllHandlersForQueryNameAndEvent('study', 'cases', EHR.Server.TriggerManager.Events.AFTER_UPSERT);
     });
 
 };
