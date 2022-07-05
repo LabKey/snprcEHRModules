35ffe91619d3c40cafcbfcae52fe726be9ab8786
Jay Allen <jallen@txbiomed.org>
2022-07-05 15:09:10 -0500

    removed project values from assignment query

--- snprc_ehr/src/org/labkey/snprc_ehr/query/QueryConstants.java
+++ snprc_ehr/src/org/labkey/snprc_ehr/query/QueryConstants.java
@@ -24,9 +24,7 @@ public class QueryConstants
     public static final String DATE_COLUMN = "date";
     public static final String DEMOGRAPHICS_TABLE = "demographics";
     public static final String ID_COLUMN = "Id";
-    public static final String PROJECTS_AT_TIME_CALCULATED = "Projects At Time";
     public static final String PROTOCOLS_AT_TIME_CALCULATED = "Protocols At Time";
-    public static final String PROJECT_NUMBERS_AT_TIME_CALCULATED = "Project Numbers At Time";
     public static final String PRIMARY_KEY_VARIABLE = "primaryKeyColumnName";
     public static final String SCHEMA_VARIABLE = "schemaName";
     public static final String EHR_PATH_VARIABLE = "ehrPath";
@@ -37,9 +35,7 @@ public class QueryConstants
 
     public static final String ASSIGNMENT_AT_TIME_SQL = "SELECT\n" +
             "sd.${" + PRIMARY_KEY_VARIABLE + "},\n" +
-            "group_concat(DISTINCT h.project.displayName, chr(10)) as " + CaseUtils.toCamelCase(PROJECTS_AT_TIME_CALCULATED, true) + ",\n" +
             "group_concat(DISTINCT h.protocol.displayName, chr(10)) as " + CaseUtils.toCamelCase(PROTOCOLS_AT_TIME_CALCULATED, true) + ",\n" +
-            "group_concat(DISTINCT h.project.project, chr(10)) as " + CaseUtils.toCamelCase(PROJECT_NUMBERS_AT_TIME_CALCULATED, true) + "\n" +
             "FROM \"${" + SCHEMA_VARIABLE + "}\".\"${" + QUERY_VARIABLE + "}\" sd\n" +
             "JOIN \"${" + TARGET_CONTAINER_VARIABLE + "}\".study.assignment h\n" +
             "  ON (sd.id = h.id AND h.dateOnly <= CAST(sd.${" + DATE_COLUMN_VARIABLE + "} AS DATE) AND (CAST(sd.${" + DATE_COLUMN_VARIABLE +"} AS DATE) <= h.enddateCoalesced) AND h.qcstate.publicdata = true)\n" +
