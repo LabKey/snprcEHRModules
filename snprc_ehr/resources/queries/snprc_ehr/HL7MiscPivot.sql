0b508d62a3d130048323d4bcff90d807228d747c
Terry J Hawkins <thawkins@txbiomed.org>
2022-07-08 16:40:37 -0500

    - Still working on pivot queries - Begin adding ETLs to bring Sqlmed data into new table structures - Fixed FK in OBR table in metadata

--- snprc_ehr/resources/queries/snprc_ehr/HL7MiscPivot.sql
+++ snprc_ehr/resources/queries/snprc_ehr/HL7MiscPivot.sql
@@ -26,7 +26,7 @@ FROM snprc_ehr.HL7_OBR obr
 ON obr.OBJECT_ID = obx.OBR_OBJECT_ID AND obr.SET_ID = obx.OBR_SET_ID
     LEFT OUTER JOIN snprc_ehr.HL7_NTE nte ON obr.OBJECT_ID = nte.OBR_OBJECT_ID AND obr.SET_ID = nte.OBR_SET_ID
     LEFT OUTER JOIN snprc_ehr.labwork_Panels AS lp on obx.TEST_ID = lp.TestId AND obr.PROCEDURE_ID = lp.ServiceId
-WHERE obr.PROCEDURE_ID.Dataset = 'Biochemistry'
+WHERE obr.PROCEDURE_ID.Dataset = 'Misc Tests'
 
 GROUP BY obr.ANIMAL_ID, obr.OBSERVATION_DATE_TM, obr.MESSAGE_ID, obr.PROCEDURE_NAME, obr.SET_ID, COALESCE (lp.ServiceId.ServiceName, obr.PROCEDURE_NAME), obr.PROCEDURE_ID, nte.COMMENT, COALESCE (lp.TestName, obx.TEST_NAME)
     PIVOT RESULT, QUALITATIVE_RESULT, ABNORMAL_FLAGS BY TestName
@@ -34,5 +34,5 @@ GROUP BY obr.ANIMAL_ID, obr.OBSERVATION_DATE_TM, obr.MESSAGE_ID, obr.PROCEDURE_N
 --      IN
 --     (
 --     select TestName from snprc_ehr.labwork_panels t
---     where t.includeInPanel = true AND t.ServiceId.Dataset='Biochemistry'
+--     where t.includeInPanel = true AND t.ServiceId.Dataset='Misc Tests'
 --     )
