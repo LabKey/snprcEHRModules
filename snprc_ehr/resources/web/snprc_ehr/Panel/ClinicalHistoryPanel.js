dc8d1f0579697d5f596887e9ad87b6611c1e4423
jallentxbiomed <jallen@txbiomed.org>
2023-02-02 14:38:51 -0600

    fixed view name

--- snprc_ehr/resources/web/snprc_ehr/Panel/ClinicalHistoryPanel.js
+++ snprc_ehr/resources/web/snprc_ehr/Panel/ClinicalHistoryPanel.js
@@ -12,75 +12,8 @@
  * @cfg printMode
  */
 Ext4.define('SNPRC.panel.ClinicalHistoryPanel', {
-    extend: 'Ext.panel.Panel',
+    extend: 'EHR.panel.ClinicalHistoryPanel',
     alias: 'widget.snprc-clinicalhistorypanel',
-
-    showMaxDate: false,
-
-    initComponent: function(){
-        this.sortMode = this.sortMode || 'date';
-
-        if (this.minDate && !Ext4.isDate(this.minDate))
-            this.minDate = LDK.ConvertUtils.parseDate(this.minDate);
-        if (this.maxDate && !Ext4.isDate(this.maxDate))
-            this.maxDate = LDK.ConvertUtils.parseDate(this.maxDate);
-
-        Ext4.apply(this, {
-            border: false,
-            items: [
-                this.getGridConfig()
-            ],
-            listeners: {
-                scope: this,
-                render: function(){
-                    //defer loading of data
-                    if (Ext4.isDefined(store) && this.autoLoadRecords){
-                        store.reloadData({
-                            subjectIds: [this.subjectId],
-                            caseId: this.caseId,
-                            minDate: this.minDate,
-                            maxDate: this.maxDate,
-                            checkedItems: this.checkedItems
-                        });
-                    }
-                }
-            }
-        });
-
-        this.callParent();
-
-        var grid = this.down('grid');
-        if (grid.rendered){
-            grid.setLoading(true);
-        }
-        else {
-            grid.on('afterrender', function(grid){
-                if (grid.store.loading){
-                    grid.setLoading(true);
-                }
-            }, this, {delay: 120, single: true});
-        }
-
-        if(this.subjectId || this.caseId){
-            var store = this.down('#gridPanel').store;
-            store.on('datachanged', function(){
-                if (!store.loading){
-                    var grid = this.down('grid');
-                    if (grid){
-                        grid.setLoading(false);
-                        grid.getView().refresh();
-                    }
-                }
-            }, this);
-
-            store.on('exception', function(store){
-                this.down('grid').setLoading(false);
-            }, this);
-        }
-        else {
-            Ext4.Msg.alert('Error', 'Must supply at least 1 subject Id or a caseId')
-        }
-    },
 
     getGridConfig: function(){
         return {
@@ -224,41 +157,6 @@ Ext4.define('SNPRC.panel.ClinicalHistoryPanel', {
         };
     },
 
-    reloadData: function(config){
-        var grid = this.down('grid');
-
-        grid.setLoading(true);
-        grid.store.reloadData({
-            minDate: config.minDate,
-            maxDate: config.maxDate,
-            subjectIds: [this.subjectId],
-            caseId: this.caseId,
-            sortMode: this.sortMode,
-            checkedItems: this.checkedItems
-        });
-    },
-
-    doReload: function(){
-        var minDateField = this.down('#minDate');
-        var maxDateField = this.down('#maxDate');
-        if (!minDateField.isValid()){
-            Ext4.Msg.alert('Error', 'Invalid value for min date');
-            return;
-        }
-        if (!maxDateField.isValid()){
-            Ext4.Msg.alert('Error', 'Invalid value for max date');
-            return;
-        }
-
-        this.minDate = minDateField.getValue();
-        this.maxDate = maxDateField.getValue();
-
-        this.reloadData({
-            minDate: this.minDate,
-            maxDate: this.maxDate
-        });
-    },
-
     doBetweenDates: function(minDate, maxDate){
         var minDateField = this.down('#minDate');
         var maxDateField = this.down('#maxDate');
@@ -270,15 +168,6 @@ Ext4.define('SNPRC.panel.ClinicalHistoryPanel', {
             minDate: this.minDate,
             maxDate: this.maxDate
         });
-    },
-
-    getStoreConfig: function(){
-        return {
-            type: 'ehr-clinicalhistorystore',
-            containerPath: this.containerPath,
-            redacted: this.redacted,
-            sortMode: this.sortMode
-        };
     },
 
     getColumnConfig: function(){
@@ -316,57 +205,4 @@ Ext4.define('SNPRC.panel.ClinicalHistoryPanel', {
         }];
     },
 
-    getGroupingFeature: function(){
-        return Ext4.create('Ext.grid.feature.Grouping', {
-            groupHeaderTpl: [
-                '<div>{name:this.formatName}</div>', {
-                    formatName: function(name) {
-                        name = name.split('_');
-                        var id = name.shift();
-                        var date = name[0].split("-");
-                        var date2 = new Date(date[0], date[1]-1, date[2], 0, 0, 0);
-                        return id + ' (' +  Ext4.util.Format.date(date2, LABKEY.extDefaultDateFormat)  + ')';
-                    }
-                }
-            ],
-            hideGroupedHeader: true,
-            startCollapsed: false,
-            id: 'historyGrouping'
-        });
-    },
-
-    changeMode: function(mode){
-        this.sortMode = mode;
-        var grid = this.down('grid');
-
-        var columns = this.getColumnConfig();
-        Ext4.Array.forEach(columns, function(col){
-            if (col.dataIndex == 'date'){
-                col.hidden = (mode == 'date');
-            }
-            else if (col.dataIndex == 'timeString'){
-                col.hidden = (mode == 'type');
-            }
-        }, this);
-
-        grid.on('reconfigure', function(){
-            grid.store.changeMode(mode);
-        }, this, {single: true});
-        grid.reconfigure(null, columns);
-    },
-
-    showFilterPanel: function(){
-        Ext4.create('EHR.window.ClinicalHistoryFilterWindow', {
-            clinicalHistoryPanel: this
-        }).show();
-    },
-
-    applyFilter: function(types){
-        this.checkedItems = types;
-        this.getStore().applyFilter(types);
-    },
-
-    getStore: function(){
-        return this.down('grid').store;
-    }
 });
