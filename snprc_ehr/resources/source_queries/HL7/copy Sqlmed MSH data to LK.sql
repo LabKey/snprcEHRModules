e415b6746ab66cfb91e55abd4128891646f442fd
Terry J Hawkins <thawkins@txbiomed.org>
2023-03-01 10:34:28 -0600

    Retargeted migration queries to use SSMS linked server and added a cutoff date for incremental updates. (#568)

--- snprc_ehr/resources/source_queries/HL7/copy Sqlmed MSH data to LK.sql
+++ snprc_ehr/resources/source_queries/HL7/copy Sqlmed MSH data to LK.sql
@@ -3,8 +3,12 @@
 -- Create date: 7-11-2022
 -- Description:	Copy MSH records from Sqlmed data to populate LabKey HL7 tables
 -- Changes:
+-- 02/28/2023: Retargeted query to use linked server and added a cutoff date
 -- ==========================================================================================
 BEGIN TRAN 
+DECLARE @obr_date DATE
+SET @obr_date = '02/28/2023'
+
 INSERT INTO labkey.snprc_ehr.HL7_MSH
 (
     MESSAGE_ID,
@@ -37,14 +41,20 @@ INSERT INTO labkey.snprc_ehr.HL7_MSH
 		   animal.dbo.f_map_username(obr.USER_NAME) AS USER_NAME,
 		   obr.ENTRY_DATE_TM AS ENTRY_DATE_TM,
 		   c.EntityId AS container
-	FROM animal.dbo.CLINICAL_PATH_OBR AS obr
+	FROM [FROGSTAR\VORTEX].animal.dbo.CLINICAL_PATH_OBR AS obr
 
 		-- select primates only from the TxBiomed colony
-		INNER JOIN [animal].labkey_etl.V_DEMOGRAPHICS AS d ON d.id = obr.ANIMAL_ID
-		LEFT OUTER JOIN [animal].dbo.TAC_COLUMNS AS tc ON tc.object_id = obr.OBJECT_ID
+		INNER JOIN [FROGSTAR\VORTEX].[animal].labkey_etl.V_DEMOGRAPHICS AS d ON d.id = obr.ANIMAL_ID
+		LEFT OUTER JOIN [FROGSTAR\VORTEX].[animal].dbo.TAC_COLUMNS AS tc ON tc.object_id = obr.OBJECT_ID
 		INNER JOIN labkey.core.Containers AS c ON c.Name = 'SNPRC'
 	WHERE obr.RESULT_STATUS IN ( 'F', 'C', 'D' )
 		  AND obr.VERIFIED_DATE_TM IS NOT NULL
-		  AND NOT EXISTS (SELECT 1 FROM animal.labkey_etl.v_delete_clinPathRuns AS dcpr WHERE obr.OBJECT_ID = dcpr.objectid)
+		  AND NOT EXISTS (SELECT 1 FROM [FROGSTAR\VORTEX].animal.labkey_etl.v_delete_clinPathRuns AS dcpr WHERE obr.OBJECT_ID = dcpr.objectid)
+
+	AND obr.MESSAGE_ID IN (
+		SELECT MESSAGE_ID FROM [FROGSTAR\VORTEX].animal.dbo.CLINICAL_PATH_OBR
+		WHERE CAST(ENTRY_DATE_TM AS DATE) = @obr_date
+	)
+
 )
-COMMIT
+--COMMIT
