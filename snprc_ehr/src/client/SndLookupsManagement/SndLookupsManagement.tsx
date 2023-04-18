ff3d0eedd633506c911bac8dbda50591ee5f8662
jallentxbiomed <jallen@txbiomed.org>
2023-04-18 14:50:57 -0500

    modularized react components

--- snprc_ehr/src/client/SndLookupsManagement/SndLookupsManagement.tsx
+++ snprc_ehr/src/client/SndLookupsManagement/SndLookupsManagement.tsx
@@ -1,11 +1,64 @@
-import React, { FC} from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import './styles/sndLookupsManagement.scss'
 
-import { LookupSetsGridPanel } from './components/LookupSetsGridPanel';
+import {
+    InjectedQueryModels,
+    isLoading,
+    withQueryModels
+} from '@labkey/components';
+import { Col, Row } from 'react-bootstrap';
+import { TableGridPanel } from './components/TableGridPanel';
+import { SCHEMAS } from '../schemas';
+import { Filter } from '@labkey/api';
 
-export const SndLookupsManagement: FC = React.memo(props => {
+export const SndLookupsManagementImpl: FC<InjectedQueryModels> = React.memo(props => {
+    const [lookupSetId, setLookupSetId] = useState<string>('');
+    const { actions, queryModels } = props;
+
+    const handleSelectedParentRow = (id: string): void => {
+        setLookupSetId(id);
+    }
+
+    const onSuccess = (response: any) => {
+        let message, error;
+        if (response) {
+            let count = 0;
+
+        }
+    }
     return (
         <div>
-            <LookupSetsGridPanel />
+            <Row>
+                <Col xs={10} md={4} className={"sidenav"} >
+                    <TableGridPanel table={"lookupSets"}
+                                    rowIdName={"lookupSetId"}
+                                    actions={actions}
+                                    omittedColumns={['label', 'description', 'Folder', 'createdby', 'created', 'modifiedby', 'modified', 'objectid']}
+                                    queryModels={queryModels}
+                                    schemaQuery={SCHEMAS.SND_TABLES.LOOKUP_SETS}
+                                    title={"Lookup Key"}
+                                    handleSelectedParentRow={handleSelectedParentRow}
+                                    onChange={onSuccess}
+                                    onCreate={onSuccess}
+                                    filters={[]}/>
+                </Col>
+                <Col xs={10} md={7}>
+                    <TableGridPanel table={"lookups"}
+                                    rowIdName={"lookupId"}
+                                    actions={actions}
+                                    omittedColumns={['label', 'description', 'folder', 'createdby', 'created', 'modifiedby', 'modified', 'objectid', 'sortOrder']}
+                                    queryModels={queryModels}
+                                    schemaQuery={SCHEMAS.SND_TABLES.LOOKUPS}
+                                    title={"Lookup Value"}
+                                    parentId={lookupSetId}
+                                    onChange={onSuccess}
+                                    onCreate={onSuccess}
+                                    filters={[Filter.create('lookupSetId', lookupSetId)] }
+                                     />
+                </Col>
+            </Row>
         </div>
     );
 });
+
+export const SndLookupsManagement = withQueryModels<{}>(SndLookupsManagementImpl);
--- snprc_ehr/src/client/SndLookupsManagement/components/LookupSetsGridPanel.tsx
+++ snprc_ehr/src/client/SndLookupsManagement/components/LookupSetsGridPanel.tsx
@@ -2,18 +2,19 @@ import React, { FC, useEffect, useRef, useState } from 'react';
 import {
     GridPanel,
     InjectedQueryModels, isLoading, LoadingSpinner,
-    LoadingState,
+    LoadingState, ManageDropdownButton, QueryModel, SelectionMenuItem,
     withQueryModels
 } from '@labkey/components';
 import { SCHEMAS } from '../../schemas';
 import { LookupsGridPanel } from './LookupsGridPanel';
-import { Col, Row } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 import '../styles/sndLookupsManagement.scss'
 import { ManageButtons } from './ManageButtons';
 
 const LookupSetsGridPanelImpl: FC<InjectedQueryModels> = React.memo(props => {
     const { actions, queryModels } = props;
     const [lookupSetId, setLookupSetId] = useState<string>('');
+    const [showDialog, setShowDialog] = useState<string>('');
 
     useEffect(() => {
         setLastSelectedLookupSetId();
@@ -59,6 +60,54 @@ const LookupSetsGridPanelImpl: FC<InjectedQueryModels> = React.memo(props => {
         return selectedLookupSetIds.size > 0 ? Array.from(selectedLookupSetIds).pop() : undefined;
     }
 
+    const toggleDialog = (name: string, requiresSelection = false): void => {
+        if (requiresSelection && queryModels['lookupSets'].hasSelections) {
+            setShowDialog(undefined);
+        } else {
+            setShowDialog(name);
+        }
+    }
+
+    const closeDialog = (): void => {
+        toggleDialog(undefined);
+    }
+
+    const onCreateComplete = (response: any): void => {
+        closeDialog();
+        onRowSelectionChange(queryModels['lookupSets'], undefined, false);
+        reloadLookupSetModel();
+    }
+
+    const reloadLookupSetModel = (): void => {
+        actions.loadModel('lookupSets', true);
+    }
+
+    const onRowSelectionChange = (model: QueryModel, row: any, checked: boolean): void => {
+        let selectedId;
+
+        if (checked) {
+            if (row) {
+                selectedId = row.getIn(['lookupSetId', 'value']);
+            } else if (model.hasSelections) {
+                selectedId = getLastSelectedLookupSetId();
+            }
+        }
+        updateSelectedLookupSetId(selectedId);
+    }
+
+    const renderButtons = () => {
+        const model = queryModels['lookupSets'];
+        return (
+            <div className="btn-group">
+                {<Button bsStyle={'success'}>Create</Button>}
+                <ManageDropdownButton id={`lookupSets`}>
+                    <SelectionMenuItem id={"delete-menu-item"} text={"Delete"} queryModel={model} />
+                    <SelectionMenuItem id={"edit-menu-item"} text={"Edit"} queryModel={model} />
+                </ManageDropdownButton>
+            </div>
+        )
+    }
+
     return (
         <div>
             <Row>
@@ -72,7 +121,7 @@ const LookupSetsGridPanelImpl: FC<InjectedQueryModels> = React.memo(props => {
                                    showPagination={false}
                                    allowSelections={true}
                                    title={"Lookup Key"}
-                                   ButtonsComponent={ManageButtons}
+                                   ButtonsComponent={renderButtons}
 
                         />
                     )}
