b9744a3c701e80154455cad7478f2d5e6abb3532
jallentxbiomed <jallen@txbiomed.org>
2023-06-27 11:29:58 -0500

    changed dev.jsx to dev.tsx for entryPoints

---
+++ snprc_ehr/src/client/BirthRecordReport/dev.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+
+import BirthRecordReport from './BirthRecordReport'
+
+const render = () => {
+    ReactDOM.render(
+      <AppContainer>
+        <BirthRecordReport />
+      </AppContainer>,
+        document.getElementById('app')
+    )
+}
+
+declare const module: any;
+if (module.hot) {
+    module.hot.accept()
+}
+
+render()
---
+++ snprc_ehr/src/client/ChipReader/dev.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+
+import ChipReader from './ChipReader'
+
+const render = () => {
+    ReactDOM.render(
+      <AppContainer>
+        <ChipReader />
+      </AppContainer>,
+        document.getElementById('app')
+    )
+}
+
+declare const module: any;
+if (module.hot) {
+    module.hot.accept()
+}
+
+render()
