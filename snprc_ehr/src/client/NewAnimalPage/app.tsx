dad2bdd295d243b46c399c075aef46684e0b4470
labkey-martyp <martyp@labkey.com>
2023-06-27 08:44:49 -0700

    Typescript interactions

--- snprc_ehr/src/client/NewAnimalPage/app.tsx
+++ snprc_ehr/src/client/NewAnimalPage/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { NewAnimalPage } from "./NewAnimalPage";
 
-import NewAnimalPage from './NewAnimalPage'
 
 // Need to wait for container element to be available in labkey wrapper before render
 window.addEventListener('DOMContentLoaded', () => {
