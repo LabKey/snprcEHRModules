1f47bb809f06fc9891aadee96cdde14b4bb6cb85
jallentxbiomed <jallen@txbiomed.org>
2023-07-20 14:21:16 -0500

    added mount to test

--- snprc_ehr/src/client/SndLookupsManagement/__tests__/SndLookupsManagement.test.tsx
+++ snprc_ehr/src/client/SndLookupsManagement/__tests__/SndLookupsManagement.test.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { SndLookupsManagement } from '../SndLookupsManagement';
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom'
 
-describe('SndLookupsManagement', () => {
-    it('renders the SndLookupsManagement page', () => {
-        render(<SndLookupsManagement />);
-        expect(screen.getByTestId('lookup-sets')).toBeInTheDocument();
-        expect(screen.getByTestId('lookups')).toBeInTheDocument();
+describe('SndLookupsManagement',  () => {
+    it('renders the SndLookupsManagement page', async () => {
+        render(<SndLookupsManagement/>);
+        await waitFor(() =>  {
+            expect(screen.getByTestId('lookup-sets')).toBeInTheDocument();
+            expect(screen.getByTestId('lookups')).toBeInTheDocument();
+        })
+
+
     })
 })
