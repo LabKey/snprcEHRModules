ab64eb9c71d6d9bd4b2533a9ea857946b96060c7
labkey-martyp <martyp@labkey.com>
2023-08-03 19:40:58 -0700

    Handle query view

--- snprc_ehr/src/client/SndLookupsManagement/schemas.ts
+++ snprc_ehr/src/client/SndLookupsManagement/schemas.ts
@@ -2,8 +2,8 @@ import { SchemaQuery } from '@labkey/components';
 
 const SND_SCHEMA = 'snd';
 export const SND_TABLES = {
-    LOOKUPS: SchemaQuery.create(SND_SCHEMA, 'lookups'),
-    LOOKUP_SETS: SchemaQuery.create(SND_SCHEMA, 'lookupSets'),
+    LOOKUPS: new SchemaQuery(SND_SCHEMA, 'lookups'),
+    LOOKUP_SETS: new SchemaQuery(SND_SCHEMA, 'lookupSets'),
     SCHEMA: SND_SCHEMA
 };
 
