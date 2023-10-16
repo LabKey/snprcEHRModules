24b43e2d58bca9007002b0be307d7215ab7d39c9
Spamhurts <thawkins@txbiomed.org>
2023-10-16 15:24:47 -0500

    - Reformatted SQL files

--- snprc_ehr/resources/source_queries/fGetAllSuperPkgs.sql
+++ snprc_ehr/resources/source_queries/fGetAllSuperPkgs.sql
@@ -1,9 +1,7 @@
-USE animal
-GO
 /****** Object:  UserDefinedFunction snd.fGetAllSuperPkgs    Script Date: 10/6/2023 10:36:51 AM ******/
-SET ANSI_NULLS ON
+SET ANSI_NULLS ON;
 GO
-SET QUOTED_IDENTIFIER ON
+SET QUOTED_IDENTIFIER ON;
 GO
 ALTER FUNCTION labkey_etl.fGetAllSuperPkgs
 ()
@@ -13,7 +11,7 @@ RETURNS @expandedSuperPackages TABLE
     SuperPkgId INTEGER NOT NULL,
     ParentSuperPkgId INTEGER NULL,
     PkgId INTEGER NOT NULL,
-	TreePath VARCHAR(MAX) NOT NULL,
+    TreePath VARCHAR(MAX) NOT NULL,
     SuperPkgPath VARCHAR(MAX) NOT NULL,
     SortOrder INTEGER NULL,
     Required INTEGER NULL,
@@ -22,74 +20,77 @@ RETURNS @expandedSuperPackages TABLE
     Active INTEGER NOT NULL,
     Repeatable INTEGER NOT NULL,
     Level INTEGER NOT NULL,
-	Created DATETIME NOT NULL,
-	CreatedBy VARCHAR(MAX) NOT NULL,
-	Modified DATETIME NOT NULL,
-	ModifiedBy VARCHAR(MAX) NOT NULL
+    Created DATETIME NOT NULL,
+    CreatedBy VARCHAR(MAX) NOT NULL,
+    Modified DATETIME NOT NULL,
+    ModifiedBy VARCHAR(MAX) NOT NULL
 )
 AS
 BEGIN
     DECLARE @loopCursor CURSOR;
     DECLARE @topLevelPkgId INTEGER;
 
-SET @loopCursor = CURSOR LOCAL FOR
-SELECT Pkg_Id AS topLevelPackageId
-FROM dbo.v_superPackageParents AS tl
-WHERE tl.PARENT_SUPER_PKG_ID IS NULL
+    SET @loopCursor = CURSOR LOCAL FOR
+    SELECT  Pkg_Id AS topLevelPackageId
+    FROM
+        dbo.v_superPackageParents AS tl
+    WHERE
+        tl.PARENT_SUPER_PKG_ID IS NULL
     FOR READ ONLY;
 
-OPEN @loopCursor;
-FETCH @loopCursor
+    OPEN @loopCursor;
+    FETCH @loopCursor
     INTO @topLevelPkgId;
 
-WHILE (@@FETCH_STATUS = 0)
-BEGIN
-INSERT INTO @expandedSuperPackages
-(
-    TopLevelPkgId,
-    SuperPkgId,
-    ParentSuperPkgId,
-    PkgId,
-	treePath,
-    SuperPkgPath,
-    SortOrder,
-    Required,
-    DESCRIPTION,
-    Narrative,
-    Active,
-    Repeatable,
-    Level,
-	Created,
-	CreatedBy,
-	Modified,
-	ModifiedBy
-)
-    (SELECT TopLevelPkgId,
+    WHILE (@@FETCH_STATUS = 0)
+    BEGIN
+        INSERT INTO @expandedSuperPackages
+        (
+            TopLevelPkgId,
             SuperPkgId,
             ParentSuperPkgId,
             PkgId,
-			SuperPkgPath,
+            TreePath,
             SuperPkgPath,
             SortOrder,
             Required,
-            Description,
+            DESCRIPTION,
             Narrative,
             Active,
             Repeatable,
             Level,
-			Created,
-			dbo.f_map_username(CreatedBy) AS CreatedBy,
-			Modified,
-			dbo.f_map_username(ModifiedBy) AS ModifiedBy
-     FROM labkey_etl.fGetSuperPkg(@topLevelPkgId) );
+            Created,
+            CreatedBy,
+            Modified,
+            ModifiedBy
+        )
+        (SELECT TopLevelPkgId,
+                SuperPkgId,
+                ParentSuperPkgId,
+                PkgId,
+                SuperPkgPath,
+                SuperPkgPath,
+                SortOrder,
+                Required,
+                Description,
+                Narrative,
+                Active,
+                Repeatable,
+                Level,
+                Created,
+                dbo.f_map_username(CreatedBy)  AS CreatedBy,
+                Modified,
+                dbo.f_map_username(ModifiedBy) AS ModifiedBy
+         FROM
+                labkey_etl.fGetSuperPkg(@topLevelPkgId) );
 
-FETCH NEXT FROM @loopCursor
-    INTO @topLevelPkgId;
+        FETCH NEXT FROM @loopCursor
+        INTO @topLevelPkgId;
 
-END;
+    END;
 
-CLOSE @loopCursor;
-DEALLOCATE @loopCursor;
+    CLOSE @loopCursor;
+    DEALLOCATE @loopCursor;
 
-       RETURN;
+    RETURN;
 END;
--- snprc_ehr/resources/source_queries/fGetSuperPkgs.sql
+++ snprc_ehr/resources/source_queries/fGetSuperPkgs.sql
@@ -1,96 +1,120 @@
 /****** Object:  UserDefinedFunction snd.fGetSuperPkg    Script Date: 10/5/2023 4:58:55 PM ******/
-SET ANSI_NULLS ON
+SET ANSI_NULLS ON;
 GO
-SET QUOTED_IDENTIFIER ON
+SET QUOTED_IDENTIFIER ON;
 GO
-ALTER FUNCTION labkey_etl.fGetSuperPkg ( @TopLevelPkgId INT )
+ALTER FUNCTION labkey_etl.fGetSuperPkg
+(
+    @TopLevelPkgId INT
+)
 RETURNS TABLE
 AS
-
 RETURN
+(
+    WITH CTE1 (TopLevelPkgId, SuperPkgId, ParentSuperPkgId, PkgId, SuperPkgPath, SortOrder, Required, DESCRIPTION,
+               Narrative, Active, Repeatable, Level, Modified, ModifiedBy, Created, CreatedBy, ObjectId
+              )
+    AS
     (
-   WITH    CTE1 ( TopLevelPkgId, SuperPkgId, ParentSuperPkgId, PkgId, SuperPkgPath, SortOrder, Required, DESCRIPTION, Narrative, Active, Repeatable, Level,
-				  Modified, ModifiedBy, Created, CreatedBy, ObjectId)
-              AS (
-
-
-                   SELECT   @TopLevelPkgId AS TopLevelPkgId ,
-                            sp.Super_Pkg_Id AS SuperPkgId ,
-                            spp.PARENT_SUPER_PKG_ID AS ParentSuperPkgId ,
-                            sp.Pkg_Id AS PkgId ,
-                            RIGHT(SPACE(3)
-                                  + CONVERT(VARCHAR(MAX), ROW_NUMBER() OVER ( ORDER BY sp.ORDER_NUM )),
-                                  3) AS SuperPkgPath,
-                            COALESCE(sp.ORDER_NUM, 0) AS SortOrder ,
-                            0 AS Required ,
-                            p.Description AS Description ,
-                            p.Narrative AS Narrative ,
-                            CASE WHEN p.DISPLAYABLE_FLAG = 'Y' THEN 1 ELSE 0 END AS Active ,
-                            CASE WHEN p.REPEATABLE_FLAG = 'Y' THEN 1 ELSE 0 END AS Repeatable ,
-                            1 AS Level,
-							COALESCE(tc.created, sp.ENTRY_DATE_TM) AS Created,
-							COALESCE(tc.createdby, sp.USER_NAME) AS CreatedBy,
-							sp.ENTRY_DATE_TM AS Modified,
-							sp.USER_NAME AS ModifiedBy,
-							sp.OBJECT_ID AS ObjectId
-                   FROM     dbo.Super_Pkgs sp
-				   INNER JOIN dbo.PKGS AS p ON p.PKG_ID = sp.PKG_ID
-                   INNER JOIN dbo.v_sndSuperPackageParents AS spp ON sp.SUPER_PKG_ID = spp.SUPER_PKG_ID
-				   LEFT JOIN dbo.TAC_COLUMNS AS tc ON sp.OBJECT_ID = tc.object_id
-                   WHERE    sp.Pkg_Id = @TopLevelPkgId
-                            AND sp.PARENT_PKG_ID IS NULL
-                   UNION ALL
-                   SELECT   @TopLevelPkgId AS TopLevelPkgId ,
-                            sp.Super_Pkg_Id AS SuperPkgId ,
-                            c.SuperPkgId AS ParentSuperPkgId ,
-                            sp.Pkg_Id AS PkgId ,
-                            c.SuperPkgPath + '/' + RIGHT(SPACE(3)
-                                                     + CONVERT(VARCHAR(MAX), RIGHT(ROW_NUMBER() OVER ( ORDER BY sp.ORDER_NUM ),
-                                                              10)), 3) AS SuperPkgPath ,
-                            sp.ORDER_NUM AS SortOrder ,
-                            0 AS Required ,
-                            p.Description AS Description ,
-                            p.Narrative AS Narrative ,
-                            CASE WHEN p.DISPLAYABLE_FLAG = 'Y' THEN 1 ELSE 0 END AS Active ,
-                            CASE WHEN p.REPEATABLE_FLAG = 'Y' THEN 1 ELSE 0 END AS Repeatable ,
-                            c.Level + 1 AS Level,
-							c.created AS Created,
-							c.createdby AS CreatedBy,
-							c.Modified AS Modified,
-							c.ModifiedBy AS ModifiedBy,
-							sp.Object_Id AS ObjectId
-                   FROM     dbo.SUPER_PKGS AS sp
-				   INNER JOIN dbo.PKGS AS p ON sp.Pkg_Id = p.Pkg_Id
-				   INNER JOIN dbo.v_sndSuperPackageParents AS spp ON sp.SUPER_PKG_ID = spp.SUPER_PKG_ID
-                   INNER JOIN CTE1 AS c ON spp.PARENT_SUPER_PKG_ID = c.SuperPkgId
-						  OR EXISTS (SELECT  1
-							FROM dbo.v_sndSuperPackageParents AS spp2
-							WHERE c.PkgId = spp2.Pkg_Id AND spp2.PARENT_SUPER_PKG_ID IS NULL
-							AND spp.PARENT_SUPER_PKG_ID = spp.SUPER_PKG_ID
-							)
-
-                 )
-    SELECT  @TopLevelPkgId AS TopLevelPkgId ,
-            c.SuperPkgId AS SuperPkgId ,
-            c.ParentSuperPkgId AS ParentSuperPkgId ,
-            c.PkgId AS PkgId ,
-            c.SuperPkgPath AS SuperPkgPath ,
-            c.SortOrder AS SortOrder ,
-            c.Required AS Required ,
-            c.DESCRIPTION AS Description ,
-            c.Narrative AS Narrative ,
-            c.Active AS Active ,
-            c.Repeatable AS Repeatable ,
+    SELECT  @TopLevelPkgId AS TopLevelPkgId,
+            sp.SUPER_PKG_ID AS SuperPkgId,
+            spp.PARENT_SUPER_PKG_ID AS ParentSuperPkgId,
+            sp.PKG_ID AS PkgId,
+            RIGHT(SPACE(3) + CONVERT(   VARCHAR(MAX),
+                                        ROW_NUMBER() OVER (ORDER BY
+                                                               sp.ORDER_NUM
+                                                          )
+                                    ), 3) AS SuperPkgPath,
+            COALESCE(sp.ORDER_NUM, 0) AS SortOrder,
+            0 AS Required,
+            p.DESCRIPTION AS Description,
+            p.NARRATIVE AS Narrative,
+            CASE WHEN p.DISPLAYABLE_FLAG = 'Y'
+                      THEN 1
+                ELSE  0
+            END AS Active,
+            CASE WHEN p.REPEATABLE_FLAG = 'Y'
+                      THEN 1
+                ELSE  0
+            END AS Repeatable,
+            1 AS Level,
+            COALESCE(tc.created, sp.ENTRY_DATE_TM) AS Created,
+            COALESCE(tc.createdby, sp.USER_NAME) AS CreatedBy,
+            sp.ENTRY_DATE_TM AS Modified,
+            sp.USER_NAME AS ModifiedBy,
+            sp.OBJECT_ID AS ObjectId
+    FROM
+        dbo.SUPER_PKGS sp
+        INNER JOIN dbo.PKGS AS p ON p.PKG_ID = sp.PKG_ID
+        INNER JOIN dbo.v_sndSuperPackageParents AS spp ON sp.SUPER_PKG_ID = spp.SUPER_PKG_ID
+        LEFT JOIN dbo.TAC_COLUMNS AS tc ON sp.OBJECT_ID = tc.object_id
+    WHERE
+        sp.PKG_ID = @TopLevelPkgId AND  sp.PARENT_PKG_ID IS NULL
+    UNION ALL
+    SELECT  @TopLevelPkgId AS TopLevelPkgId,
+            sp.SUPER_PKG_ID AS SuperPkgId,
+            c.SuperPkgId AS ParentSuperPkgId,
+            sp.PKG_ID AS PkgId,
+            c.SuperPkgPath + '/'
+            + RIGHT(SPACE(3) + CONVERT(   VARCHAR(MAX),
+                                          RIGHT(ROW_NUMBER() OVER (ORDER BY
+                                                                       sp.ORDER_NUM
+                                                                  ), 10)
+                                      ), 3) AS SuperPkgPath,
+            sp.ORDER_NUM AS SortOrder,
+            0 AS Required,
+            p.DESCRIPTION AS Description,
+            p.NARRATIVE AS Narrative,
+            CASE WHEN p.DISPLAYABLE_FLAG = 'Y'
+                      THEN 1
+                ELSE  0
+            END AS Active,
+            CASE WHEN p.REPEATABLE_FLAG = 'Y'
+                      THEN 1
+                ELSE  0
+            END AS Repeatable,
+            c.Level + 1 AS Level,
+            c.Created AS Created,
+            c.CreatedBy AS CreatedBy,
+            c.Modified AS Modified,
+            c.ModifiedBy AS ModifiedBy,
+            sp.OBJECT_ID AS ObjectId
+    FROM
+        dbo.SUPER_PKGS AS sp
+        INNER JOIN dbo.PKGS AS p ON sp.PKG_ID = p.PKG_ID
+        INNER JOIN dbo.v_sndSuperPackageParents AS spp ON sp.SUPER_PKG_ID = spp.SUPER_PKG_ID
+        INNER JOIN CTE1 AS c ON spp.PARENT_SUPER_PKG_ID = c.SuperPkgId
+                                OR   EXISTS
+                                     (
+                                         SELECT     1
+                                         FROM
+                                                dbo.v_sndSuperPackageParents AS spp2
+                                         WHERE
+                                             c.PkgId = spp2.PKG_ID AND   spp2.PARENT_SUPER_PKG_ID IS NULL
+                                             AND spp.PARENT_SUPER_PKG_ID = spp.SUPER_PKG_ID
+                                     )
+    )
+    SELECT  @TopLevelPkgId AS TopLevelPkgId,
+            c.SuperPkgId AS SuperPkgId,
+            c.ParentSuperPkgId AS ParentSuperPkgId,
+            c.PkgId AS PkgId,
+            c.SuperPkgPath AS SuperPkgPath,
+            c.SortOrder AS SortOrder,
+            c.Required AS Required,
+            c.DESCRIPTION AS Description,
+            c.Narrative AS Narrative,
+            c.Active AS Active,
+            c.Repeatable AS Repeatable,
             c.Level AS Level,
-			c.Created,
-			c.CreatedBy,
-			c.Modified,
-			c.ModifiedBy,
-			c.ObjectId
-    FROM    CTE1 c
-
+            c.Created,
+            c.CreatedBy,
+            c.Modified,
+            c.ModifiedBy,
+            c.ObjectId
+    FROM
+        CTE1 c
 );
 GO
 
-GRANT SELECT ON Labkey_etl.fGetSuperPkg TO z_labkey
+GRANT SELECT ON labkey_etl.fGetSuperPkg TO z_labkey;
 GO
