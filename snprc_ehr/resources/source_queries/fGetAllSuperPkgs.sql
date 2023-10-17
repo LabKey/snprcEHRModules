4ca292019b1b0e655765aceca0ac61346ec7ff1a
Spamhurts <thawkins@txbiomed.org>
2023-10-17 11:13:59 -0500

    - added missing column and fixed join view name

--- snprc_ehr/resources/source_queries/fGetAllSuperPkgs.sql
+++ snprc_ehr/resources/source_queries/fGetAllSuperPkgs.sql
@@ -1,9 +1,9 @@
-/****** Object:  UserDefinedFunction snd.fGetAllSuperPkgs    Script Date: 10/6/2023 10:36:51 AM ******/
+/****** Object:  UserDefinedFunction [labkey_etl].[fGetAllSuperPkgs]    Script Date: 10/17/2023 11:09:59 AM ******/
 SET ANSI_NULLS ON;
 GO
 SET QUOTED_IDENTIFIER ON;
 GO
-ALTER FUNCTION labkey_etl.fGetAllSuperPkgs
+ALTER FUNCTION [labkey_etl].[fGetAllSuperPkgs]
 ()
 RETURNS @expandedSuperPackages TABLE
 (
@@ -23,7 +23,8 @@ RETURNS @expandedSuperPackages TABLE
     Created DATETIME NOT NULL,
     CreatedBy VARCHAR(MAX) NOT NULL,
     Modified DATETIME NOT NULL,
-    ModifiedBy VARCHAR(MAX) NOT NULL
+    ModifiedBy VARCHAR(MAX) NOT NULL,
+    ObjectId UNIQUEIDENTIFIER NOT NULL
 )
 AS
 BEGIN
@@ -31,66 +32,68 @@ BEGIN
     DECLARE @topLevelPkgId INTEGER;
 
     SET @loopCursor = CURSOR LOCAL FOR
-    SELECT  Pkg_Id AS topLevelPackageId
-    FROM
-        dbo.v_superPackageParents AS tl
-    WHERE
-        tl.PARENT_SUPER_PKG_ID IS NULL
+SELECT  PKG_ID AS topLevelPackageId
+FROM
+    dbo.v_sndSuperPackageParents AS tl
+WHERE
+    tl.PARENT_SUPER_PKG_ID IS NULL
     FOR READ ONLY;
 
-    OPEN @loopCursor;
-    FETCH @loopCursor
+OPEN @loopCursor;
+FETCH @loopCursor
     INTO @topLevelPkgId;
 
-    WHILE (@@FETCH_STATUS = 0)
-    BEGIN
-        INSERT INTO @expandedSuperPackages
-        (
-            TopLevelPkgId,
+WHILE (@@FETCH_STATUS = 0)
+BEGIN
+INSERT INTO @expandedSuperPackages
+(
+    TopLevelPkgId,
+    SuperPkgId,
+    ParentSuperPkgId,
+    PkgId,
+    TreePath,
+    SuperPkgPath,
+    SortOrder,
+    Required,
+    DESCRIPTION,
+    Narrative,
+    Active,
+    Repeatable,
+    Level,
+    Created,
+    CreatedBy,
+    Modified,
+    ModifiedBy,
+    ObjectId
+)
+    (SELECT TopLevelPkgId,
             SuperPkgId,
             ParentSuperPkgId,
             PkgId,
-            TreePath,
+            SuperPkgPath,
             SuperPkgPath,
             SortOrder,
             Required,
-            DESCRIPTION,
+            Description,
             Narrative,
             Active,
             Repeatable,
             Level,
             Created,
-            CreatedBy,
+            dbo.f_map_username(CreatedBy) AS CreatedBy,
             Modified,
-            ModifiedBy
-        )
-        (SELECT TopLevelPkgId,
-                SuperPkgId,
-                ParentSuperPkgId,
-                PkgId,
-                SuperPkgPath,
-                SuperPkgPath,
-                SortOrder,
-                Required,
-                Description,
-                Narrative,
-                Active,
-                Repeatable,
-                Level,
-                Created,
-                dbo.f_map_username(CreatedBy)  AS CreatedBy,
-                Modified,
-                dbo.f_map_username(ModifiedBy) AS ModifiedBy
-         FROM
-                labkey_etl.fGetSuperPkg(@topLevelPkgId) );
+            dbo.f_map_username(ModifiedBy) AS ModifiedBy,
+            ObjectId
+     FROM
+         labkey_etl.fGetSuperPkg(@topLevelPkgId) );
 
-        FETCH NEXT FROM @loopCursor
-        INTO @topLevelPkgId;
+FETCH NEXT FROM @loopCursor
+    INTO @topLevelPkgId;
 
-    END;
+END;
 
-    CLOSE @loopCursor;
-    DEALLOCATE @loopCursor;
+CLOSE @loopCursor;
+DEALLOCATE @loopCursor;
 
     RETURN;
 END;
