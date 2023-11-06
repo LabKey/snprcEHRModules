4077cb916066e02c44dacc25ad5ab6c10df1a7b4
jallentxbiomed <jallen@txbiomed.org>
2023-11-06 08:36:48 -0600

    added procedure entry to widget page

--- snprc_ehr/src/client/SndEventsWidget/SndEventsWidget.tsx
+++ snprc_ehr/src/client/SndEventsWidget/SndEventsWidget.tsx
@@ -1,10 +1,13 @@
-import React, { FC, memo, useEffect, useMemo, useState } from 'react';
+import React, { FC, memo, useState } from 'react';
 import { EventListingGridPanel } from './components/EventListingGridPanel';
 
 export const SndEventsWidget: FC = memo(props => {
+
+    const [subjectID, setSubjectID] = useState<string>('42409');
+
     return (
         <div>
-            <EventListingGridPanel subjectID={1512572} />
+            <EventListingGridPanel subjectID={subjectID} />
         </div>
     )
 })
--- snprc_ehr/src/client/SndEventsWidget/components/ProcedureEntryModal.tsx
+++ snprc_ehr/src/client/SndEventsWidget/components/ProcedureEntryModal.tsx
@@ -1,25 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { FC, memo, useEffect } from 'react';
+import { Modal } from 'react-bootstrap';
+import { Alert, WizardNavButtons } from '@labkey/components';
 
-export const ProcedureEntryModal = () => {
-    const [eventId, setEventId] = useState('');
-
+interface Props {
+    show: boolean,
+    onCancel: () => any,
+    eventId: string
+}
+export const ProcedureEntryModal: FC<Props> = memo((props: Props) => {
+    const { show, onCancel, eventId } = props;
     useEffect(() => {
         const widget = document.createElement('script');
-
-        widget.src = "/labkey/snprc_mobile/app/widget.js";
+        widget.src = '/labkey/snprc_mobile/app/widget.js'
         widget.type = "application/javascript";
         widget.async = true;
         document.body.appendChild(widget);
-    }, [])
-
-    const updateReport = () => {
         const ProcedureEntryWidget = document.getElementById('procedure-entry-widget')
         ProcedureEntryWidget.setAttribute('data-event-id', eventId)
-    }
+
+    }, [eventId])
 
     return (
-        <div id ="fake-root">
-            <div id="procedure-entry-widget" data-event-id={eventId}></div>
-        </div>
+        <Modal show={show} onHide={onCancel} className={'edit-modal'}>
+            <Modal.Header closeButton>
+                <Modal.Title>Procedure Entry</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div id="procedure-entry-widget" data-event-id={eventId}></div>
+            </Modal.Body>
+            <Modal.Footer>
+                {/*<WizardNavButtons containerClassName={''}*/}
+                {/*                  cancel={onCancel}*/}
+                {/*                  finish={table == 'Lookup' || row?.['IsInUse'] == 'false'}*/}
+                {/*                  finishText={'Finish'}*/}
+                {/*                  isFinishing={isSubmitting}*/}
+                {/*                  isFinishingText={`Updating ${table}s`}*/}
+                {/*                  nextStep={table == 'Lookup' || row?.['IsInUse'] == 'false' ? handleUpdate : onCancel}*/}
+                {/*/>*/}
+            </Modal.Footer>
+        </Modal>
+
     )
-}
+});
