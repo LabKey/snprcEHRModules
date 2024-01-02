7430738d2f3bff1472ec11c40d7e12497feac6a5
Adam Rauch <adam@labkey.com>
2024-01-01 23:33:07 -0500

    Update schema version to 24.000 (#664)

--- snprc_ehr/src/org/labkey/snprc_ehr/SNPRC_EHRModule.java
+++ snprc_ehr/src/org/labkey/snprc_ehr/SNPRC_EHRModule.java
@@ -118,7 +118,7 @@ public class SNPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 23.002;
+        return 24.000;
     }
 
     @Override
