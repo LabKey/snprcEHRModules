89b9c647a58518ae2aea55ebbf3669454904eeee
Terry J Hawkins <thawkins@txbiomed.org>
2024-04-05 16:26:21 -0500

    - Updated metadata for the therapy drug list (formulary) tables and related lookups to allow nulls in the RowId and ObjectID columns during data entry - Added trigger scripts to maintain RowIds and ObjectIds in the formulary and related tables - Added Java helper method to retrieve the next RowId value from the passed in table name

---
+++ snprc_ehr/resources/queries/snprc_ehr/therapy_formulary.js
@@ -0,0 +1,23 @@
+require("ehr/triggers").initScript(this);
+
+var LABKEY = require("labkey");
+var snprcTriggerHelper = new org.labkey.snprc_ehr.query.SNPRC_EHRTriggerHelper(LABKEY.Security.currentUser.id, LABKEY.Security.currentContainer.id);
+
+function onInsert(helper, scriptErrors, row) {
+
+    // add RowId if it doesn't exist
+    if (row.RowId === undefined) {
+        let rowid = snprcTriggerHelper.getNextTherapyId("therapy_formulary");
+
+        if (rowid === undefined || rowid < 1) {
+            console.log("Error retrieving next RowId.");
+            EHR.Server.Utils.addError(scriptErrors, "RowId", "Error retrieving next RowId.", "ERROR");
+        }
+        else
+        {
+            row.RowId = rowid;
+        }
+    }
+    // add ObjectId if it doesn't exist
+    row.ObjectId = row.ObjectId || LABKEY.Utils.generateUUID().toUpperCase();
+};
