f84fd48c09f31afe4dcf18df22699ca16014d10f
cnathe <cnathe@labkey.com>
2024-07-09 11:08:18 -0500

    Update entryPoints.js to use permissionClasses instead of permission property

--- snprc_ehr/src/client/entryPoints.js
+++ snprc_ehr/src/client/entryPoints.js
@@ -7,35 +7,35 @@ module.exports = {
     apps: [{
         name: 'NewAnimalPage',
         title: 'New Animal Wizard',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/NewAnimalPage'
     }, {
         name: 'BirthRecordReport',
         title: 'Birth Record Report',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/BirthRecordReport'
     }, {
         name: 'ChipReader',
         title: 'Chip Reader',
-        permission: 'read',
+        permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
         path: './src/client/ChipReader'
     },
         {
             name: 'SsrsReporting',
             title: 'PDF Reports',
-            permission: 'read',
+            permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
             path: './src/client/SsrsReporting'
         },
         {
             name: 'SndLookupsManagement',
             title: 'SND Lookups Management',
-            permission: 'read',
+            permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
             path: './src/client/SndLookupsManagement'
         },
         {
             name: 'SndEventsWidget',
             title: 'Animal Events',
-            permission: 'read',
+            permissionClasses: ['org.labkey.api.security.permissions.ReadPermission'],
             path: './src/client/SndEventsWidget'
         },
         {
