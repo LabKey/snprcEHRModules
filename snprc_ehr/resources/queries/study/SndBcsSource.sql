6f05bbe3e5850d91f2929dcccda36ae83e10ecc7
Terry J Hawkins <thawkins@txbiomed.org>
2025-02-26 10:08:13 -0600

    Updates to support SND datasets integration (#793)
    * Updates to support SND datasets integration.
    1. Fixed how AdmitChargerIdProtocol handles edge cases for date/datetime conversion
    2. Added source queries for snd datasets: TB, BCS, Weight, Alopecia, Blood, Body Temperature, Vitals
    3. Added new dataset "BodyTemperature"
    4. Added query metadata for datasets
    5. updated browse all datasets page
    6. Changed currentBloodDraws.sql to use table names instead of labels
    7. removed physical datasets from datasets manifest and metadata files: blood, tb, weight, vitals, and Alopecia
    8. Hid unused ETLs using standalone="false":Alopecia, tb, vitals, weight
    9. Changed batch size on SndEvents ETL to 1000
    10. Added BodyTemperature tab to animal history
    11. removed javascript triggers for old study datasets: vitals, tb, alopecia
    12. replaced BodyConditionScores.sql with snd dataset
    
    * changed polling interval to 30 minutes for SND queue ETL
    
    * Fixed typo
    
    * added blood to the list of hidden ETLs
    
    * Added table title to spf_results and changed tab name in Animal History at user's request.

---
+++ snprc_ehr/resources/queries/study/SndBcsSource.sql
@@ -0,0 +1,34 @@
+SELECT
+    b.LSID as _key,
+    b.SubjectId as participantid,
+    b.date,
+    l.value as BCS,
+    CAST(SUBSTRING(l.value, 1, locate(' -', l.value ,1)) AS varchar(4))  AS BCSValue,
+    p.ReferenceId as project,
+    null as taskid,
+    null as requestid,
+    null as performedby,
+    null as description,
+    null as remark,
+    b.QcState,
+    b.SequenceNum,
+    b.ObjectId,
+    b.EventDataId,
+    b.EventId,
+    b.SuperPkgId,
+    b.ObjectURI,
+    b.Container,
+    b.ParentEventDataId,
+    b.SortOrder,
+    b.USDACode,
+    b.LSID,
+    e.CreatedBy,
+    e.Created,
+    e.ModifiedBy,
+    e.Modified
+
+FROM SND.Categories.BCS as b
+         INNER JOIN SND.LookupSets as ls on ls.SetName = 'BCS'
+         INNER JOIN SND.Lookups as l on l.lookupSetId = ls.lookupSetId
+         INNER JOIN SND.Events as e on b.EventId = e.EventId
+         INNER JOIN SND.Projects as p on e.ParentObjectId = p.objectId
---
+++ snprc_ehr/resources/queries/study/SndBloodSource.sql
@@ -0,0 +1,37 @@
+SELECT
+    b.LSID as _key,
+    b.SubjectId as participantid,
+    b.date,
+    b.QcState,
+    b.SequenceNum,
+    b.ObjectId,
+    b.EventDataId,
+    b.EventId,
+    b.SuperPkgId,
+    b.ObjectURI,
+    b.Container,
+    b.ParentEventDataId,
+    b.SortOrder,
+    b.USDACode,
+    b.reason as remark,
+    null as tube_type,
+    null as tube_vol,
+    null as num_tubes,
+    b.BLOOD_Volume as quantity,
+    null as additionalServices,
+    null as instructions,
+    null as daterequested,
+    null as BloodRemaining,
+    p.ReferenceId as project,
+    null as taskid,
+    null as parentId,
+    null as requestId,
+    b.LSID,
+    e.CreatedBy,
+    e.Created,
+    e.ModifiedBy,
+    e.Modified
+
+FROM SND.Categories."Cumulative Blood" as b
+         INNER JOIN SND.Events as e on b.EventId = e.EventId
+         INNER JOIN SND.Projects as p on e.ParentObjectId = p.objectId
