66b77fa63197d8ff1c82775903aa45dbc3006e68
Terry J Hawkins <thawkins@txbiomed.org>
2025-03-07 10:31:47 -0600

    Removed debug code in javascript trigger.

--- snprc_ehr/resources/queries/study/SPF_Results.js
+++ snprc_ehr/resources/queries/study/SPF_Results.js
@@ -2,13 +2,5 @@ var console = require("console");
 var LABKEY = require("labkey");
 
 function beforeInsert(row, errors) {
-    console.log("beforeInsert");
     row.objectid = row.objectid || LABKEY.Utils.generateUUID().toUpperCase();
-    console.log("Insert spf_results.js:  row.Sample_number = " + row.Sample_number + " -- row.objectid = " + row.objectid)
-}
-
-function beforeUpdate(row, errors) {
-    console.log("beforeUpdate");
-    row.objectid = row.objectid || LABKEY.Utils.generateUUID().toUpperCase();
-    console.log("Update spf_results.js:  row.Sample_number = " + row.Sample_number + " -- row.objectid = " + row.objectid)
 }
