4bafb75992b5eda7aa114bef4bf2139f975b041c
Terry J Hawkins <thawkins@txbiomed.org>
2025-12-17 15:36:54 -0600

    Fixed issues related to primary key issues in the super package staging table. (#866)

--- snprc_ehr/src/org/labkey/snprc_ehr/SNPRC_EHRModule.java
+++ snprc_ehr/src/org/labkey/snprc_ehr/SNPRC_EHRModule.java
@@ -118,7 +118,7 @@ public class SNPRC_EHRModule extends ExtendedSimpleModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 25.000;
+        return 25.001;
     }
 
     @Override
