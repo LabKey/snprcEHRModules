ca217d6a8f2781e7a626bc8bc3da14700d27eb32
Terry J Hawkins <thawkins@txbiomed.org>
2025-12-23 13:54:55 -0600

    Add earliest housing demographics provider (#868)
    * Added earliest housing demographics provider and query.
    
    * removed redundant column wrapping method and replaced "Id" strings with a constant
    
    * actually removed the redundant column wrapping method that was missed on the last commit and aligned wrapped column name with the demographic provider
    
    * - Use the ID_COLUMN constant in QueryConstants.java.
     - Renamed local wrapped column var name.
    - added offspring to the browse all datasets screen
    
    * commented out earliest housing registration to sort out test issue

---
+++ snprc_ehr/resources/queries/study/demographicsEarliestHousing.sql
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2017-2018 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
+SELECT
+    d.Id AS Id,
+    h.date as FirstHousingDate,
+    h.room as FirstHousingRoom,
+    h.room.area.description as FirstHousingRoomDescription
+FROM study.demographics d
+
+
+--date of first housing
+LEFT JOIN study.housing as h on h.id = d.id and h.qcstate.publicdata = true
+    and h.date = (select min(c1.date) from study.housing as c1
+                   where h.id = c1.id)
