2f1f868fcc9bd676110d688cd2f337e33f543296
Adam Rauch <adam@labkey.com>
2025-12-31 16:49:20 -0800

    Bump schema versions to 26.000
    Original PR: https://github.com/LabKey/ehrModules/pull/1081

--- snd/src/org/labkey/snd/SNDModule.java
+++ snd/src/org/labkey/snd/SNDModule.java
@@ -67,7 +67,7 @@ public class SNDModule extends SpringModule
     @Override
     public @Nullable Double getSchemaVersion()
     {
-        return 25.000;
+        return 26.000;
     }
 
     @Override
