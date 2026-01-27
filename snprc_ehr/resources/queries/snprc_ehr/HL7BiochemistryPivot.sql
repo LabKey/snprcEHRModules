98ddb957b2221450cfa65e59f1a193e1a65dc4d0
Spamhurts <thawkins@txbiomed.org>
2026-01-27 16:18:11 -0600

    Added Routine Chemistry query view

--- snprc_ehr/resources/queries/snprc_ehr/HL7BiochemistryPivot.sql
+++ snprc_ehr/resources/queries/snprc_ehr/HL7BiochemistryPivot.sql
@@ -21,6 +21,6 @@ GROUP BY obr.ANIMAL_ID, obr.OBSERVATION_DATE_TM, obr.MESSAGE_ID, COALESCE (lp.Se
 
     IN
     (
-    select DISTINCT TestName as TEST_NAME from snprc_ehr.labwork_panels t
+    select DISTINCT ltrim(Rtrim(TestName)) as TEST_NAME from snprc_ehr.labwork_panels t
     where t.includeInPanel = true AND t.ServiceId.Dataset='Biochemistry'
     )
