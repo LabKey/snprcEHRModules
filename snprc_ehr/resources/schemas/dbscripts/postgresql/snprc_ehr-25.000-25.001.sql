bfb73b3ed0d181310acfdea7b9a89a1bd29fa09f
Adam Rauch <adam@labkey.com>
2026-02-12 17:56:28 -0800

    snprc_ehr PostgreSQL initial support

---
+++ snprc_ehr/resources/schemas/dbscripts/postgresql/snprc_ehr-25.000-25.001.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS snprc_ehr.SndSuperPackageStaging;
+
+CREATE TABLE snprc_ehr.SndSuperPackageStaging
+(
+    TopLevelPkgId INTEGER NOT NULL,
+    SuperPkgId INTEGER NOT NULL,
+    ParentSuperPkgId INTEGER NULL,
+    PkgId INTEGER NOT NULL,
+    TreePath VARCHAR(800) NOT NULL,
+    SuperPkgPath TEXT NOT NULL,
+    SortOrder INTEGER NULL,
+    Required INTEGER NULL,
+    Description TEXT NOT NULL,
+    Narrative TEXT NOT NULL,
+    Active INTEGER NOT NULL,
+    Repeatable INTEGER NOT NULL,
+    Level INTEGER NOT NULL,
+    Created TIMESTAMP NOT NULL,
+    CreatedBy INTEGER NOT NULL,
+    Modified TIMESTAMP NOT NULL,
+    ModifiedBy INTEGER NOT NULL,
+    diModified TIMESTAMP NOT NULL,
+    ObjectId ENTITYID NOT NULL,
+
+    CONSTRAINT PK_SndSuperPackageStaging PRIMARY KEY (TopLevelPkgId, SuperPkgId, TreePath)
+);
