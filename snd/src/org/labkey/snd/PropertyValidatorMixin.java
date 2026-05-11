0a9bff24fef62253c4098feda116aae9094170a1
Josh Eckels <jeckels@labkey.com>
2026-05-11 16:53:27 -0700

    Update JSON serialization for GWT removal (#930)

--- snd/src/org/labkey/snd/PropertyValidatorMixin.java
+++ snd/src/org/labkey/snd/PropertyValidatorMixin.java
@@ -19,9 +19,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * Configures the fields that are not returned when serializing a GWTPropertyValidator.
- * Ideally we would just add the @JsonIgnore annotations to GWTPropertyValidator directly,
- * but the GWT compiler would need to have jackson on the classpath which isn't
- * necessary.
+ * Ideally we would just add the @JsonIgnore annotations to GWTPropertyValidator directly.
  */
 @JsonIgnoreProperties({
         "type"  // Done outside jackson
