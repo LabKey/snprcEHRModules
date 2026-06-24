560ddf7c1d4febeca6b26483ba75312c662b97c0
Adam Rauch <adam@labkey.com>
2026-06-24 11:21:54 -0700

    Drop redundant index (#953)

---
+++ snprc_scheduler/resources/schemas/dbscripts/postgresql/snprc_scheduler-26.000-26.001.sql
@@ -0,0 +1,2 @@
+-- Dropping NONUNIQUE idx_timelineprojectitemfk1 [TimelineObjectId] because it overlaps with PRIMARY pk_timelineprojectitem [TimelineObjectId, ProjectItemId]
+DROP INDEX snprc_scheduler.idx_timelineprojectitemfk1;
