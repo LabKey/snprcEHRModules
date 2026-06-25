2d978b2be736f9ee360b71fda88386a8d69e0b21
jallentxbiomed <jallen@txbiomed.org>
2026-06-25 10:59:44 -0500

    added fix for date shifting on save

--- snprc_ehr/src/org/labkey/snprc_ehr/domain/AnimalGroup.java
+++ snprc_ehr/src/org/labkey/snprc_ehr/domain/AnimalGroup.java
@@ -17,6 +17,7 @@ package org.labkey.snprc_ehr.domain;
 
 import org.json.JSONObject;
 import org.labkey.api.data.Entity;
+import org.labkey.api.util.DateUtil;
 
 import java.util.Date;
 
@@ -111,6 +112,9 @@ public class AnimalGroup extends Entity
 
     public JSONObject toJSON()
     {
-        return new JSONObject(this);
+        JSONObject json = new JSONObject(this);
+        json.put("date",    date    == null ? null : DateUtil.toISO(date).substring(0, 10));
+        json.put("endDate", endDate == null ? null : DateUtil.toISO(endDate).substring(0, 10));
+        return json;
     }
 }
