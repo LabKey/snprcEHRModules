e19a751bd5b11a08456b283925067323b52d45fb
Marty Pradere <martyp@labkey.com>
2026-09-17 20:06:50 -0600

    Move snprc_ehr npm build to @labkey/build 7 and @labkey/eslint-config
    Replaces the module's own eslint 7 and airbnb config with @labkey/eslint-config, drops the react-hot-loader entry wrappers that @labkey/build no longer supports, pins cheerio so the lock can be regenerated from package.json, and regenerates the lock.

--- snprc_ehr/src/client/BirthRecordReport/dev.tsx
+++ snprc_ehr/src/client/BirthRecordReport/dev.tsx
@@ -1,22 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
 
 import BirthRecordReport from './BirthRecordReport'
 
 const render = () => {
-    ReactDOM.render(
-      <AppContainer>
-        <BirthRecordReport />
-      </AppContainer>,
-        document.getElementById('app')
-    )
-}
-
-declare const module: any;
-
-if (module.hot) {
-    module.hot.accept()
+    ReactDOM.render(<BirthRecordReport />, document.getElementById('app'))
 }
 
 render()
